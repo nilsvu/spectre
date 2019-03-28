e009fe4b85aed8df65a7edf1dfcafc5bd91378cb
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2019-03-28 18:56:51 +0100

    FIX Add action to compute nonlinear operator

--- src/Elliptic/Actions/ComputeOperatorAction.hpp
+++ src/Elliptic/Actions/ComputeOperatorAction.hpp
@@ -8,6 +8,8 @@
 #include "DataStructures/DataBox/DataBox.hpp"
 #include "DataStructures/DataBox/DataBoxTag.hpp"
 #include "DataStructures/DataBox/Prefixes.hpp"
+#include "NumericalAlgorithms/LinearSolver/Tags.hpp"
+#include "NumericalAlgorithms/NonlinearSolver/Tags.hpp"
 #include "Utilities/Gsl.hpp"
 #include "Utilities/Requires.hpp"
 #include "Utilities/TMPL.hpp"
@@ -73,8 +75,35 @@ struct ComputeOperatorAction {
     // consistency with other actions that do the same (for instance
     // `dg::Actions::ApplyFluxes` that is not specific to elliptic systems)
     db::mutate_apply<db::split_tag<db::add_tag_prefix<
-                         Metavariables::temporal_id::template step_prefix,
+                         LinearSolver::Tags::OperatorAppliedTo,
                          typename system::variables_tag>>,
+                     typename computer::argument_tags>(computer{},
+                                                       make_not_null(&box));
+    return std::forward_as_tuple(std::move(box));
+  }
+};
+struct ComputeNonlinearOperatorAction {
+  template <typename DbTagsList, typename... InboxTags, typename Metavariables,
+            typename ArrayIndex, typename ActionList,
+            typename ParallelComponent,
+            Requires<tmpl::size<DbTagsList>::value != 0> = nullptr>
+  static auto apply(db::DataBox<DbTagsList>& box,
+                    tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
+                    const Parallel::ConstGlobalCache<Metavariables>& /*cache*/,
+                    const ArrayIndex& /*array_index*/,
+                    const ActionList /*meta*/,
+                    const ParallelComponent* const /*meta*/) noexcept {
+    using system = typename Metavariables::system;
+    using computer = typename system::compute_nonlinear_operator_action;
+    // Notes:
+    // - step_variables is not zeroed and the operator cannot assume this.
+    // - We retrieve the `step_prefix` from the `Metavariables` (as opposed to
+    // hard-coding `LinearSolver::Tags::OperatorAppliedTo`) to retain
+    // consistency with other actions that do the same (for instance
+    // `dg::Actions::ApplyFluxes` that is not specific to elliptic systems)
+    db::mutate_apply<db::split_tag<db::add_tag_prefix<
+                         NonlinearSolver::Tags::OperatorAppliedTo,
+                         typename system::nonlinear_fields_tag>>,
                      typename computer::argument_tags>(computer{},
                                                        make_not_null(&box));
     return std::forward_as_tuple(std::move(box));
