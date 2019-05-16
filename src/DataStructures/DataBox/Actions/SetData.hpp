434f6e3d3fb44d3657272ae41004d4c4d782d549
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2019-05-16 15:26:06 +0200

    Add SetData simple action
    Takes a tagged tuple and mutates the DataBox with its content

---
+++ src/DataStructures/DataBox/Actions/SetData.hpp
@@ -0,0 +1,45 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "Parallel/ConstGlobalCache.hpp"
+
+namespace db {
+namespace Actions {
+
+/*!
+ * \ingroup ActionsGroup
+ * \brief Mutate the DataBox tags in `TagsList` according to the `data`.
+ *
+ * DataBox changes:
+ * - Modifies:
+ *   - All tags in `TagsList`
+ */
+template <typename TagsList>
+struct SetData {
+  template <typename... DbTags, typename... InboxTags, typename Metavariables,
+            typename ArrayIndex, typename ActionList,
+            typename ParallelComponent,
+            Requires<sizeof...(DbTags) != 0> = nullptr>
+  static auto apply(
+      db::DataBox<tmpl::list<DbTags...>>& box,
+      const tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
+      const Parallel::ConstGlobalCache<Metavariables>& /*cache*/,
+      const ArrayIndex& array_index, const ActionList /*meta*/,
+      const ParallelComponent* const /*meta*/,
+      tuples::tagged_tuple_from_typelist<TagsList> data) noexcept {
+    tmpl::for_each<TagsList>([&box, &data ](auto tag_v) noexcept {
+      using tag = tmpl::type_from<decltype(tag_v)>;
+      db::mutate<tag>(
+          make_not_null(&box), [&data](const gsl::not_null<db::item_type<tag>*>
+                                           value) noexcept {
+            *value = std::move(get<tag>(data));
+          });
+    });
+  }
+};
+
+}  // namespace Actions
+}  // namespace db
