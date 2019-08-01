f3fb9b95b70bfdb072f230ce7b8bd5432503684c
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2019-08-01 13:36:03 +0200

    Allow InitializeFluxes to skip inhomogeneous BC

--- src/Elliptic/DiscontinuousGalerkin/Actions/InitializeFluxes.hpp
+++ src/Elliptic/DiscontinuousGalerkin/Actions/InitializeFluxes.hpp
@@ -29,7 +29,42 @@ namespace elliptic {
 namespace dg {
 namespace Actions {
 
+namespace detail {
+template <size_t Dim, typename System, bool Enabled>
+struct InhomogeneousBoundaryComputeTags {
+  using type = db::AddComputeTags<>;
+};
+template <size_t Dim, typename System>
+struct InhomogeneousBoundaryComputeTags<Dim, System, true> {
+  using analytic_fields_tag =
+      db::add_tag_prefix<::Tags::Analytic, typename System::fields_tag>;
+  using type = db::AddComputeTags<
+      // We slice the analytic solutions to the interior boundary and
+      // compute their normal-dot-fluxes for imposing inhomogeneous
+      // Dirichlet boundary conditions.
+      ::Tags::Slice<::Tags::BoundaryDirectionsInterior<Dim>,
+                    analytic_fields_tag>,
+      ::Tags::InterfaceComputeItem<::Tags::BoundaryDirectionsInterior<Dim>,
+                                   typename System::compute_analytic_fluxes>,
+      ::Tags::InterfaceComputeItem<
+          ::Tags::BoundaryDirectionsInterior<Dim>,
+          ::Tags::ComputeNormalDotFlux<analytic_fields_tag, Dim,
+                                       Frame::Inertial>>>;
+};
+}  // namespace detail
+
+// System is needed for boundary conditions
+template <typename BoundaryScheme, typename System,
+          bool PrepareInhomogenousBoundaryConditions = true>
 struct InitializeFluxes {
+ private:
+  static constexpr size_t Dim = BoundaryScheme::volume_dim;
+  using vars_tag = typename BoundaryScheme::variables_tag;
+  using fluxes_tag = db::add_tag_prefix<::Tags::Flux, vars_tag,
+                                        tmpl::size_t<Dim>, Frame::Inertial>;
+  using div_fluxes_tag = db::add_tag_prefix<::Tags::div, fluxes_tag>;
+
+ public:
   template <typename DbTagsList, typename... InboxTags, typename Metavariables,
             typename ArrayIndex, typename ActionList,
             typename ParallelComponent>
@@ -38,61 +73,49 @@ struct InitializeFluxes {
                     const Parallel::ConstGlobalCache<Metavariables>& /*cache*/,
                     const ArrayIndex& /*array_index*/, ActionList /*meta*/,
                     const ParallelComponent* const /*meta*/) noexcept {
-    using system = typename Metavariables::system;
-    constexpr size_t Dim = system::volume_dim;
-    using vars_tag = typename system::variables_tag;
-    using fluxes_tag = db::add_tag_prefix<::Tags::Flux, vars_tag,
-                                          tmpl::size_t<Dim>, Frame::Inertial>;
-    using div_fluxes_tag = db::add_tag_prefix<::Tags::div, fluxes_tag>;
-    using analytic_fields_tag =
-        db::add_tag_prefix<::Tags::Analytic, typename system::fields_tag>;
-
     using simple_tags = db::AddSimpleTags<>;
-    using compute_tags = db::AddComputeTags<
-        // We slice the fluxes and their divergences to all interior faces
-        ::Tags::Slice<::Tags::InternalDirections<Dim>, fluxes_tag>,
-        ::Tags::Slice<::Tags::BoundaryDirectionsInterior<Dim>, fluxes_tag>,
-        ::Tags::Slice<::Tags::InternalDirections<Dim>, div_fluxes_tag>,
-        ::Tags::Slice<::Tags::BoundaryDirectionsInterior<Dim>, div_fluxes_tag>,
-        // For the strong flux lifting scheme we need the interface normal
-        // dotted into the fluxes.
-        ::Tags::InterfaceComputeItem<
-            ::Tags::InternalDirections<Dim>,
-            ::Tags::ComputeNormalDotFlux<vars_tag, Dim, Frame::Inertial>>,
-        ::Tags::InterfaceComputeItem<
-            ::Tags::BoundaryDirectionsInterior<Dim>,
-            ::Tags::ComputeNormalDotFlux<vars_tag, Dim, Frame::Inertial>>,
-        // We slice the analytic solutions to the interior boundary and compute
-        // their normal-dot-fluxes for imposing inhomogeneous Dirichlet boundary
-        // conditions.
-        ::Tags::Slice<::Tags::BoundaryDirectionsInterior<Dim>,
-                      analytic_fields_tag>,
-        ::Tags::InterfaceComputeItem<::Tags::BoundaryDirectionsInterior<Dim>,
-                                     typename system::compute_analytic_fluxes>,
-        ::Tags::InterfaceComputeItem<
-            ::Tags::BoundaryDirectionsInterior<Dim>,
-            ::Tags::ComputeNormalDotFlux<analytic_fields_tag, Dim,
-                                         Frame::Inertial>>,
-        // We mirror the system variables to the exterior (ghost) faces to
-        // impose homogeneous (zero) boundary conditions. Non-zero boundary
-        // conditions are handled as contributions to the source term during
-        // initialization.
-        ::Tags::Slice<::Tags::BoundaryDirectionsInterior<Dim>, vars_tag>,
-        ::Tags::InterfaceComputeItem<
-            ::Tags::BoundaryDirectionsExterior<Dim>,
-            ::Tags::MirrorVariables<
-                Dim, ::Tags::BoundaryDirectionsInterior<Dim>, vars_tag,
-                typename system::primal_variables>>,
-        // On exterior (ghost) boundary faces we compute the fluxes from the
-        // data that is being mirrored there to impose homogeneous Dirichlet
-        // boundary conditions. Then, we compute their normal-dot-fluxes. The
-        // flux divergences are sliced from the volume.
-        ::Tags::InterfaceComputeItem<::Tags::BoundaryDirectionsExterior<Dim>,
-                                     typename system::compute_fluxes>,
-        ::Tags::InterfaceComputeItem<
-            ::Tags::BoundaryDirectionsExterior<Dim>,
-            ::Tags::ComputeNormalDotFlux<vars_tag, Dim, Frame::Inertial>>,
-        ::Tags::Slice<::Tags::BoundaryDirectionsExterior<Dim>, div_fluxes_tag>>;
+    using compute_tags = tmpl::append<
+        tmpl::list<
+            // We slice the fluxes and their divergences to all interior
+            // faces
+            ::Tags::Slice<::Tags::InternalDirections<Dim>, fluxes_tag>,
+            ::Tags::Slice<::Tags::BoundaryDirectionsInterior<Dim>, fluxes_tag>,
+            ::Tags::Slice<::Tags::InternalDirections<Dim>, div_fluxes_tag>,
+            ::Tags::Slice<::Tags::BoundaryDirectionsInterior<Dim>,
+                          div_fluxes_tag>,
+            // For the strong flux lifting scheme we need the interface
+            // normal dotted into the fluxes.
+            ::Tags::InterfaceComputeItem<
+                ::Tags::InternalDirections<Dim>,
+                ::Tags::ComputeNormalDotFlux<vars_tag, Dim, Frame::Inertial>>,
+            ::Tags::InterfaceComputeItem<
+                ::Tags::BoundaryDirectionsInterior<Dim>,
+                ::Tags::ComputeNormalDotFlux<vars_tag, Dim, Frame::Inertial>>,
+            // We mirror the system variables to the exterior (ghost)
+            // faces to impose homogeneous (zero) boundary conditions.
+            // Non-zero boundary conditions are handled as contributions
+            // to the source term during initialization.
+            ::Tags::Slice<::Tags::BoundaryDirectionsInterior<Dim>, vars_tag>,
+            ::Tags::InterfaceComputeItem<
+                ::Tags::BoundaryDirectionsExterior<Dim>,
+                ::Tags::MirrorVariables<
+                    Dim, ::Tags::BoundaryDirectionsInterior<Dim>, vars_tag,
+                    typename System::primal_variables>>,
+            // On exterior (ghost) boundary faces we compute the fluxes
+            // from the data that is being mirrored there to impose
+            // homogeneous Dirichlet boundary conditions. Then, we
+            // compute their normal-dot-fluxes. The flux divergences are
+            // sliced from the volume.
+            ::Tags::InterfaceComputeItem<
+                ::Tags::BoundaryDirectionsExterior<Dim>,
+                typename System::compute_fluxes>,
+            ::Tags::InterfaceComputeItem<
+                ::Tags::BoundaryDirectionsExterior<Dim>,
+                ::Tags::ComputeNormalDotFlux<vars_tag, Dim, Frame::Inertial>>,
+            ::Tags::Slice<::Tags::BoundaryDirectionsExterior<Dim>,
+                          div_fluxes_tag>>,
+        tmpl::type_from<detail::InhomogeneousBoundaryComputeTags<
+            Dim, System, PrepareInhomogenousBoundaryConditions>>>;
 
     return std::make_tuple(
         ::Initialization::merge_into_databox<InitializeFluxes, simple_tags,
