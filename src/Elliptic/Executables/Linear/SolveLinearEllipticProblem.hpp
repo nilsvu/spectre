773c558b170c1effde924fd3a416d0199f6b32c7
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2019-09-18 18:20:49 +0200

    Support multiple consecutive linear solves
    Initial values for a linear solve are now set in a `prepare` action
    instead of in the initializer. This allows re-setting the linear solver
    to its initial state to perform multiple consecutive solves.
    This split also makes the order of initialization actions more
    flexible, and fixes a bug where the initial residual was not
    writted to disk because it was observed before the registration phase.

--- src/Elliptic/Executables/Linear/SolveLinearEllipticProblem.hpp
+++ src/Elliptic/Executables/Linear/SolveLinearEllipticProblem.hpp
@@ -136,49 +136,50 @@ struct Metavariables {
       typename linear_solver::initialize_element,
       dg::Actions::InitializeMortars<boundary_scheme>,
       elliptic::dg::Actions::InitializeFluxes<Metavariables>,
-      // Initialization is done. Avoid introducing an extra phase by
-      // advancing the linear solver to the first step here.
-      typename linear_solver::prepare_step,
       Initialization::Actions::RemoveOptionsAndTerminatePhase>;
+
+  using build_linear_operator_actions = tmpl::list<
+      dg::Actions::CollectDataForFluxes<boundary_scheme,
+                                        ::Tags::InternalDirections<volume_dim>>,
+      dg::Actions::SendDataForFluxes<boundary_scheme>,
+      Actions::MutateApply<elliptic::FirstOrderOperator<
+          volume_dim, LinearSolver::Tags::OperatorAppliedTo,
+          typename system::variables_tag>>,
+      elliptic::dg::Actions::ImposeHomogeneousDirichletBoundaryConditions<
+          Metavariables>,
+      dg::Actions::CollectDataForFluxes<
+          boundary_scheme, ::Tags::BoundaryDirectionsInterior<volume_dim>>,
+      dg::Actions::ReceiveDataForFluxes<boundary_scheme>,
+      Actions::MutateApply<boundary_scheme>>;
 
   // Specify all parallel components that will execute actions at some point.
   using component_list = tmpl::append<
       tmpl::list<elliptic::DgElementArray<
           Metavariables,
-          tmpl::list<
-              Parallel::PhaseActions<Phase, Phase::Initialization,
-                                     initialization_actions>,
+          tmpl::list<Parallel::PhaseActions<Phase, Phase::Initialization,
+                                            initialization_actions>,
 
-              Parallel::PhaseActions<
-                  Phase, Phase::RegisterWithObserver,
-                  tmpl::list<observers::Actions::RegisterWithObservers<
-                                 observers::RegisterObservers<
-                                     LinearSolver::Tags::IterationId,
-                                     element_observation_type>>,
-                             Parallel::Actions::TerminatePhase>>,
+                     Parallel::PhaseActions<
+                         Phase, Phase::RegisterWithObserver,
+                         tmpl::list<observers::Actions::RegisterWithObservers<
+                                        observers::RegisterObservers<
+                                            LinearSolver::Tags::IterationId,
+                                            element_observation_type>>,
+                                    // We prepare the linear solve here to avoid
+                                    // adding an extra phase. We can't do it
+                                    // before registration because it
+                                    // contributes to observers.
+                                    typename linear_solver::prepare_solve,
+                                    Parallel::Actions::TerminatePhase>>,
 
-              Parallel::PhaseActions<
-                  Phase, Phase::Solve,
-                  tmpl::list<
-                      Actions::RunEventsAndTriggers,
-                      LinearSolver::Actions::TerminateIfConverged,
-                      dg::Actions::CollectDataForFluxes<
-                          boundary_scheme,
-                          ::Tags::InternalDirections<volume_dim>>,
-                      dg::Actions::SendDataForFluxes<boundary_scheme>,
-                      Actions::MutateApply<elliptic::FirstOrderOperator<
-                          volume_dim, LinearSolver::Tags::OperatorAppliedTo,
-                          typename system::variables_tag>>,
-                      elliptic::dg::Actions::
-                          ImposeHomogeneousDirichletBoundaryConditions<
-                              Metavariables>,
-                      dg::Actions::CollectDataForFluxes<
-                          boundary_scheme,
-                          ::Tags::BoundaryDirectionsInterior<volume_dim>>,
-                      dg::Actions::ReceiveDataForFluxes<boundary_scheme>,
-                      Actions::MutateApply<boundary_scheme>,
-                      typename linear_solver::perform_step,
-                      typename linear_solver::prepare_step>>>>>,
+                     Parallel::PhaseActions<
+                         Phase, Phase::Solve,
+                         tmpl::flatten<tmpl::list<
+                             typename linear_solver::prepare_step,
+                             Actions::RunEventsAndTriggers,
+                             LinearSolver::Actions::TerminateIfConverged,
+                             build_linear_operator_actions,
+                             typename linear_solver::perform_step>>>>>>,
       typename linear_solver::component_list,
       tmpl::list<observers::Observer<Metavariables>,
                  observers::ObserverWriter<Metavariables>>>;
