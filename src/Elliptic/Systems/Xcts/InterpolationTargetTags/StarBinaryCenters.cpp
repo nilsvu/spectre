7948e99599e4c1838bbda235d9330fa2a2feff7a
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2019-10-11 16:25:18 +0200

    WIP use angular velocity estimate instead of rootfind

--- src/Elliptic/Systems/Xcts/InterpolationTargetTags/StarBinaryCenters.cpp
+++ src/Elliptic/Systems/Xcts/InterpolationTargetTags/StarBinaryCenters.cpp
@@ -53,10 +53,23 @@ std::tuple<double, double, std::array<double, 2>> solve_force_balance(
                                      get<2>(shift) * get<0, 2>(shift_gradient));
   const auto& dx_shift_y = get<0, 1>(shift_gradient);
 
-  Parallel::printf("conformal_factor_pow_4: %s\n", conformal_factor_pow_4);
-  Parallel::printf("lapse: %s\n", lapse);
-  Parallel::printf("shift_square: %s\n", shift_square);
-  Parallel::printf("shift_y: %s\n", shift_y);
+  Parallel::printf("eccentricity = %e\n", eccentricity);
+  Parallel::printf("position = [%e, %e]\n", local_star_centers[0],
+                   local_star_centers[1]);
+  Parallel::printf("conformal_factor_pow_4 = [%e, %e]\n",
+                   conformal_factor_pow_4[0], conformal_factor_pow_4[1]);
+  Parallel::printf("dx_conformal_factor_pow_4 = [%e, %e]\n",
+                   dx_conformal_factor_pow_4[0], dx_conformal_factor_pow_4[1]);
+  Parallel::printf("lapse_square = [%e, %e]\n",
+  lapse_square[0], lapse_square[1]);
+  Parallel::printf("dx_lapse_square = [%e, %e]\n", dx_lapse_square[0],
+                   dx_lapse_square[1]);
+  Parallel::printf("shift_square = [%e, %e]\n", shift_square[0],
+  shift_square[1]);
+  Parallel::printf("dx_shift_square = [%e, %e]\n", dx_shift_square[0],
+                   dx_shift_square[1]);
+  Parallel::printf("shift_y = [%e, %e]\n", shift_y[0], shift_y[1]);
+  Parallel::printf("dx_shift_y = [%e, %e]\n", dx_shift_y[0], dx_shift_y[1]);
 
   Parallel::printf("initial guess for angular_velocity: %e\n",
                    angular_velocity_estimate);
@@ -72,10 +85,11 @@ std::tuple<double, double, std::array<double, 2>> solve_force_balance(
                                             dx_lapse_square,
                                             dx_shift_square,
                                             dx_shift_y};
-  const auto center_of_mass_and_angular_velocity = RootFinder::gsl_multiroot(
-      force_balance, initial_center_of_mass_and_angular_velocity, 1.e-14, 20);
-  // const std::array<double, 2> center_of_mass_and_angular_velocity{{0.,
-  // 0.}};
+//   const auto center_of_mass_and_angular_velocity = RootFinder::gsl_multiroot(
+//    force_balance, initial_center_of_mass_and_angular_velocity, 1.e-14, 20);
+  const auto center_of_mass_and_angular_velocity =
+      initial_center_of_mass_and_angular_velocity;
+  force_balance(center_of_mass_and_angular_velocity);
 
   const DataVector specific_enthalpy = get(eos.specific_enthalpy_from_density(
       Scalar<DataVector>{{{{central_rest_mass_densities[0],
@@ -86,9 +100,9 @@ std::tuple<double, double, std::array<double, 2>> solve_force_balance(
   const auto tangential_velocity = orbital::tangential_velocity(
       center_of_mass, angular_velocity, local_star_centers, eccentricity);
 
-  Parallel::printf("center_of_mass: %e\n", center_of_mass);
-  Parallel::printf("angular_velocity: %e\n", angular_velocity);
-  Parallel::printf("tangential_velocity: %s\n", tangential_velocity);
+  Parallel::printf("center_of_mass = %e\n", center_of_mass);
+  Parallel::printf("angular_velocity = %e\n", angular_velocity);
+  Parallel::printf("tangential_velocity = %s\n", tangential_velocity);
 
   // Move this elsewhere
   const auto injection_energy =
@@ -98,7 +112,8 @@ std::tuple<double, double, std::array<double, 2>> solve_force_balance(
                (shift_square + 2. * tangential_velocity * shift_y +
                 square(tangential_velocity)));
 
-  Parallel::printf("injection_energy: %s\n", injection_energy);
+  Parallel::printf("injection_energy = [%e, %e]\n", injection_energy[0],
+                   injection_energy[1]);
 
   return {center_of_mass,
           angular_velocity,
