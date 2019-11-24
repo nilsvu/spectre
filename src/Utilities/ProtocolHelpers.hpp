60651af7e961894f11ec294e1466a5ad619a062f
Nils Leif Fischer <hello@nilsleiffischer.de>
2019-11-23 18:40:50 -0800

    fixup Add CMake option to enable rigorous protocol conformance checks

--- src/Utilities/ProtocolHelpers.hpp
+++ src/Utilities/ProtocolHelpers.hpp
@@ -11,13 +11,29 @@
 // will not match private base classes (unlike std::is_base_of)
 
 // @{
-/// Checks if the `ConformingType` conforms to the `Protocol`.
-///
-/// See \ref protocols
+/*
+ * \brief Checks if the `ConformingType` conforms to the `Protocol`.
+ *
+ * By default, only checks if the class derives off the protocol to reduce
+ * compile time. Protocol conformance is tested rigorously in the unit tests
+ * instead. Set the `SPECTRE_ALWAYS_CHECK_PROTOCOL_CONFORMANCE` CMake option to
+ * always enable rigorous protocol conformance checks.
+ *
+ * \see \ref protocols
+ */
+#ifdef SPECTRE_ALWAYS_CHECK_PROTOCOL_CONFORMANCE
+template <typename ConformingType, typename Protocol>
+constexpr bool conforms_to_v =
+    cpp17::is_convertible_v<ConformingType*, Protocol*> and
+        Protocol::template is_conforming_v<ConformingType>;
+template <typename ConformingType, typename Protocol>
+using conforms_to =
+    cpp17::bool_constant<conforms_to_v<ConformingType, Protocol>>;
+#else   // SPECTRE_ALWAYS_CHECK_PROTOCOL_CONFORMANCE
 template <typename ConformingType, typename Protocol>
 using conforms_to = typename std::is_convertible<ConformingType*, Protocol*>;
-
 template <typename ConformingType, typename Protocol>
 constexpr bool conforms_to_v =
     cpp17::is_convertible_v<ConformingType*, Protocol*>;
+#endif  // SPECTRE_ALWAYS_CHECK_PROTOCOL_CONFORMANCE
 // @}
