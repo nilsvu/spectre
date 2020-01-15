88edc8d5d18e0c051cdbea339cb96865fa55cab4
Nils Leif Fischer <hello@nilsleiffischer.de>
2020-01-15 14:54:37 +0100

    Add tags for elastic constitutive relations

--- src/PointwiseFunctions/Elasticity/ConstitutiveRelations/Tags.hpp
+++ src/PointwiseFunctions/Elasticity/ConstitutiveRelations/Tags.hpp
@@ -3,37 +3,54 @@
 
 #pragma once
 
+#include <cstddef>
+#include <memory>
 #include <string>
 
 #include "DataStructures/DataBox/DataBoxTag.hpp"
-#include "DataStructures/Tensor/TypeAliases.hpp"
 #include "PointwiseFunctions/Elasticity/ConstitutiveRelations/ConstitutiveRelation.hpp"
+#include "Utilities/TMPL.hpp"
 
-/// \cond
 namespace Elasticity {
 namespace Tags {
+
+/*!
+ * \brief The elastic material's constitutive relation.
+ *
+ * \see `Elasticity::ConstitutiveRelations::ConstitutiveRelation`
+ */
 template <size_t Dim>
-struct Stress;
-}  // namespace Tags
-}  // namespace Elasticity
-namespace Tags {
-template <size_t Dim, typename Frame>
-struct Coordinates;
-}  // namespace Tags
-/// \endcond
-
-namespace Elasticity {
-namespace Tags {
-
-/// Base tag for the constitutive relation
-struct ConstitutiveRelationBase : db::BaseTag {};
-
-template <typename ConstitutiveRelationType>
-struct ConstitutiveRelation : ConstitutiveRelationBase, db::SimpleTag {
-  static constexpr OptionString help = {
-      "The constitutive relation of the elastic material"};
-  using type = ConstitutiveRelationType;
+struct ConstitutiveRelation : db::SimpleTag {
   static std::string name() noexcept { return "Material"; }
+  using type =
+      std::unique_ptr<ConstitutiveRelations::ConstitutiveRelation<Dim>>;
+};
+
+/*!
+ * \brief The elastic material's constitutive relation, loaded from options in
+ * the `ProviderOptionTag`.
+ *
+ * Retrieves the constitutive relation from the object constructed from the
+ * `ProviderOptionTag` by calling its `constitutive_relation()` member function.
+ * Also requires the `ProviderOptionTag::type` to provide a `volume_dim` static
+ * variable and a `constitutive_relation_type` type alias.
+ *
+ * The constitutive relation can be retrieved from the DataBox using its base
+ * `Elasticity::Tags::ConstitutiveRelation` tag.
+ */
+template <typename ProviderOptionTag,
+          typename ProviderType = tmpl::type_from<ProviderOptionTag>>
+struct ConstitutiveRelationFrom
+    : ConstitutiveRelation<ProviderType::volume_dim> {
+  using base = ConstitutiveRelation<ProviderType::volume_dim>;
+  using type = tmpl::type_from<base>;
+  using option_tags = tmpl::list<ProviderOptionTag>;
+
+  template <typename Metavariables>
+  static type create_from_options(const ProviderType& provider) {
+    return std::make_unique<typename ProviderType::constitutive_relation_type>(
+        provider.constitutive_relation());
+  }
 };
 
 }  // namespace Tags
--- tests/Unit/PointwiseFunctions/Elasticity/ConstitutiveRelations/Test_Tags.cpp
+++ tests/Unit/PointwiseFunctions/Elasticity/ConstitutiveRelations/Test_Tags.cpp
@@ -3,15 +3,54 @@
 
 #include "tests/Unit/TestingFramework.hpp"
 
+#include <cstddef>
 #include <string>
 
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "Parallel/ParallelComponentHelpers.hpp"
+#include "PointwiseFunctions/Elasticity/ConstitutiveRelations/ConstitutiveRelation.hpp"
+#include "PointwiseFunctions/Elasticity/ConstitutiveRelations/IsotropicHomogeneous.hpp"
 #include "PointwiseFunctions/Elasticity/ConstitutiveRelations/Tags.hpp"
 
+namespace Elasticity {
+namespace ConstitutiveRelations {
+
 namespace {
-struct SomeConstitutiveRelation;
+struct Provider {
+  static constexpr size_t volume_dim = 1;
+  using constitutive_relation_type = IsotropicHomogeneous<volume_dim>;
+  static constitutive_relation_type constitutive_relation() { return {1., 1.}; }
+};
+
+struct ProviderOptionTag {
+  using type = Provider;
+};
 }  // namespace
 
 SPECTRE_TEST_CASE("Unit.Elasticity.Tags", "[Unit][Elasticity]") {
-  CHECK(Elasticity::Tags::ConstitutiveRelation<
-            SomeConstitutiveRelation>::name() == "Material");
+  CHECK(db::tag_name<Elasticity::Tags::ConstitutiveRelation<1>>() ==
+        "Material");
+
+  {
+    INFO("ConstitutiveRelationFrom");
+    // Fake some output of option-parsing
+    const tuples::TaggedTuple<ProviderOptionTag> options{Provider{}};
+    // Dispatch the `create_from_options` function call that constructs the
+    // constitutive relation from the options.
+    const auto constructed_data = Parallel::create_from_options<NoSuchType>(
+        options,
+        tmpl::list<
+            Elasticity::Tags::ConstitutiveRelationFrom<ProviderOptionTag>>{});
+    // Since the result is a tagged tuple we can't use base tags to retrieve it.
+    // Also, `tuples::get` doesn't automatically dereference the unique_ptr.
+    const auto& constructed_constitutive_relation = tuples::get<
+        Elasticity::Tags::ConstitutiveRelationFrom<ProviderOptionTag>>(
+        constructed_data);
+    CHECK(dynamic_cast<IsotropicHomogeneous<1>&>(
+              *constructed_constitutive_relation) ==
+          IsotropicHomogeneous<1>{1., 1.});
+  }
 }
+
+}  // namespace ConstitutiveRelations
+}  // namespace Elasticity
