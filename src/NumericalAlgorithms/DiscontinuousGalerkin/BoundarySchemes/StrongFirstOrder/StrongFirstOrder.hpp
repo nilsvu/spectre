aae5d03a8353a337a2363bec7d59351961a2d3c0
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-02-04 17:51:26 +0100

    fixup move

--- src/NumericalAlgorithms/DiscontinuousGalerkin/BoundarySchemes/StrongFirstOrder/StrongFirstOrder.hpp
+++ src/NumericalAlgorithms/DiscontinuousGalerkin/BoundarySchemes/StrongFirstOrder/StrongFirstOrder.hpp
@@ -15,6 +15,7 @@
 #include "Domain/DirectionMap.hpp"
 #include "Domain/FaceNormal.hpp"
 #include "Domain/InterfaceHelpers.hpp"
+#include "NumericalAlgorithms/DiscontinuousGalerkin/BoundarySchemes/StrongFirstOrder/Equations.hpp"
 #include "NumericalAlgorithms/DiscontinuousGalerkin/LiftFlux.hpp"
 #include "NumericalAlgorithms/DiscontinuousGalerkin/MortarHelpers.hpp"
 #include "NumericalAlgorithms/DiscontinuousGalerkin/NumericalFluxes/NumericalFluxHelpers.hpp"
@@ -76,71 +77,6 @@ struct boundary_data_computer_impl<
   }
 };
 
-// Compute the boundary flux contribution and lift it to the volume.
-//
-// This computes the numerical flux minus the flux dotted into the interface
-// normal, projects the result to the face mesh if necessary, and then lifts it
-// to the volume (still presented only on the face mesh as all other points are
-// zero).
-//
-// Projection must happen after the numerical flux calculation so that the
-// elements on either side of the mortar calculate the same result.  Projection
-// must happen before flux lifting because we want the factor of the magnitude
-// of the unit normal added during the lift to cancel the Jacobian factor in
-// integrals to preserve conservation; this only happens if the two operations
-// are done on the same grid.
-template <typename VariablesTag, typename BoundaryData,
-          typename NormalDotNumericalFluxComputer, size_t Dim>
-db::const_item_type<VariablesTag> compute_boundary_flux_contribution(
-    const BoundaryData& local_boundary_data,
-    const BoundaryData& remote_boundary_data,
-    const NormalDotNumericalFluxComputer& normal_dot_numerical_flux_computer,
-    const Scalar<DataVector>& magnitude_of_face_normal,
-    const size_t extent_perpendicular_to_boundary, const Mesh<Dim>& face_mesh,
-    const Mesh<Dim>& mortar_mesh,
-    const std::array<Spectral::MortarSize, Dim>& mortar_sizes) noexcept {
-  // Compute the Tags::NormalDotNumericalFlux from local and remote data
-  db::const_item_type<
-      db::add_tag_prefix<::Tags::NormalDotNumericalFlux, VariablesTag>>
-      normal_dot_numerical_fluxes{mortar_mesh.number_of_grid_points()};
-  dg::NumericalFluxes::normal_dot_numerical_fluxes(
-      make_not_null(&normal_dot_numerical_fluxes),
-      normal_dot_numerical_flux_computer, local_boundary_data,
-      remote_boundary_data);
-
-  // Subtract the local Tags::NormalDotFlux
-  tmpl::for_each<db::get_variables_tags_list<VariablesTag>>([
-    &normal_dot_numerical_fluxes, &local_boundary_data
-  ](const auto tag_v) noexcept {
-    using tag = tmpl::type_from<decltype(tag_v)>;
-    auto& numerical_flux =
-        get<::Tags::NormalDotNumericalFlux<tag>>(normal_dot_numerical_fluxes);
-    const auto& local_flux =
-        get<::Tags::NormalDotFlux<tag>>(local_boundary_data.field_data);
-    for (size_t i = 0; i < numerical_flux.size(); ++i) {
-      numerical_flux[i] -= local_flux[i];
-    }
-  });
-
-  // TODO: do this elsewhere
-  // Check if we need to project from the mortar back to the face
-  const bool refining =
-      face_mesh != mortar_mesh or
-      std::any_of(mortar_sizes.begin(), mortar_sizes.end(),
-                  [](const Spectral::MortarSize mortar_size) noexcept {
-                    return mortar_size != Spectral::MortarSize::Full;
-                  });
-
-  // Lift flux to the volume. We still only need to provide it on the face
-  // because it is zero everywhere else.
-  return lift_flux(
-      // Project from the mortar back to the face if needed
-      refining ? project_from_mortar(normal_dot_numerical_fluxes, face_mesh,
-                                     mortar_mesh, mortar_sizes)
-               : std::move(normal_dot_numerical_fluxes),
-      extent_perpendicular_to_boundary, magnitude_of_face_normal);
-}
-
 }  // namespace StrongFirstOrder_detail
 
 /*!
@@ -156,14 +92,16 @@ template <size_t Dim, typename VariablesTag, typename NumericalFluxComputerTag,
           typename TemporalIdTag>
 struct StrongFirstOrder {
   static constexpr size_t volume_dim = Dim;
-  using temporal_id_tag = TemporalIdTag;
   using variables_tag = VariablesTag;
+  using numerical_flux_computer_tag = NumericalFluxComputerTag;
+  using temporal_id_tag = TemporalIdTag;
+
   using dt_variables_tag =
       db::add_tag_prefix<TemporalIdTag::template step_prefix, variables_tag>;
   using normal_dot_numerical_fluxes_tag =
-      db::add_tag_prefix<::Tags::NormalDotNumericalFlux, VariablesTag>;
+      db::add_tag_prefix<::Tags::NormalDotNumericalFlux, variables_tag>;
   using normal_dot_fluxes_tag =
-      db::add_tag_prefix<::Tags::NormalDotFlux, VariablesTag>;
+      db::add_tag_prefix<::Tags::NormalDotFlux, variables_tag>;
   using magnitude_of_face_normal_tag =
       ::Tags::Magnitude<::Tags::UnnormalizedFaceNormal<volume_dim>>;
 
@@ -226,8 +164,8 @@ struct StrongFirstOrder {
       const BoundaryData& remote_data = extracted_mortar_data.second;
 
       db::item_type<dt_variables_tag> lifted_data(
-          StrongFirstOrder_detail::compute_boundary_flux_contribution<
-              variables_tag>(
+          strong_first_order_boundary_flux<
+              db::get_variables_tags_list<variables_tag>>(
               local_data, remote_data, normal_dot_numerical_flux_computer,
               get<magnitude_of_face_normal_tag>(local_data.extra_data),
               volume_mesh.extents(dimension), volume_mesh.slice_away(dimension),
