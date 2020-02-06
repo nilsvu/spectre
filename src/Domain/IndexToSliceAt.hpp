834abada5f1ced91e7aee89551b233463a0e9244
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-02-06 18:35:29 +0100

    Add offset to index_to_slice_at

--- src/Domain/IndexToSliceAt.hpp
+++ src/Domain/IndexToSliceAt.hpp
@@ -13,7 +13,9 @@
 /// Finds the index in the perpendicular dimension of an element boundary
 template <size_t Dim>
 size_t index_to_slice_at(const Index<Dim>& extents,
-                         const Direction<Dim>& direction) noexcept {
-  return direction.side() == Side::Lower ? 0
-                                         : extents[direction.dimension()] - 1;
+                         const Direction<Dim>& direction,
+                         const size_t offset = 0) noexcept {
+  return direction.side() == Side::Lower
+             ? offset
+             : extents[direction.dimension()] - 1 - offset;
 }
