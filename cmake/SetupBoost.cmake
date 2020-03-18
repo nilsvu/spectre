f1ead59aebd84c98916f4fea01f9ac2002712fc3
Nils Deppe <nd357@cornell.edu>
2020-03-18 16:35:07 -0400

    Make Boost an imported library
    Since we use boost a lot, basically every library needed to be updated.

--- cmake/SetupBoost.cmake
+++ cmake/SetupBoost.cmake
@@ -3,8 +3,6 @@
 
 find_package(Boost 1.60.0 REQUIRED COMPONENTS program_options)
 
-spectre_include_directories(${Boost_INCLUDE_DIR})
-list(APPEND SPECTRE_LIBRARIES ${Boost_LIBRARIES})
 message(STATUS "Boost include: ${Boost_INCLUDE_DIRS}")
 message(STATUS "Boost libraries: ${Boost_LIBRARIES}")
 
@@ -12,3 +10,9 @@ file(APPEND
   "${CMAKE_BINARY_DIR}/LibraryVersions.txt"
   "Boost Version:  ${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION}.${Boost_SUBMINOR_VERSION}\n"
   )
+
+add_library(Boost INTERFACE IMPORTED)
+set_property(TARGET Boost
+  APPEND PROPERTY INTERFACE_LINK_LIBRARIES ${Boost_LIBRARIES})
+set_property(TARGET Boost PROPERTY
+  INTERFACE_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIR})
