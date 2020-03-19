cb130237e8c3e4a6b407f181c3af2e2f33fb80e2
Nils Deppe <nd357@cornell.edu>
2020-03-19 09:16:27 -0400

    fixup: numpy

--- cmake/SetupNumPy.cmake
+++ cmake/SetupNumPy.cmake
@@ -5,7 +5,6 @@ find_package(NumPy 1.10 REQUIRED)
 
 message(STATUS "NumPy incl: " ${NUMPY_INCLUDE_DIRS})
 message(STATUS "NumPy vers: " ${NUMPY_VERSION})
-spectre_include_directories(${NUMPY_INCLUDE_DIRS})
 
 file(APPEND
   "${CMAKE_BINARY_DIR}/LibraryVersions.txt"
