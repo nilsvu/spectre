0fde007834f949f6d4b08679074afa4cd3dae432
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-06-08 09:55:50 +0200

    WIP section reductions

---
+++ src/Parallel/SectionReductionMessage.hpp
@@ -0,0 +1,36 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include "ckmulticast.h"
+
+#include "Parallel/Algorithms/AlgorithmArrayDeclarations.hpp"
+
+template <typename SectionProxy, typename TargetProxy, typename ReductionData,
+          typename SectionIdTag>
+struct SectionReductionMessage
+    : public CkMcastBaseMsg,
+      public CMessage_SectionReductionMessage<SectionProxy, TargetProxy,
+                                              ReductionData, SectionIdTag> {
+  SectionProxy section_proxy;
+  typename SectionIdTag::type section_id;
+  TargetProxy target_proxy;
+  ReductionData data;
+  SectionReductionMessage(SectionProxy local_section_proxy,
+                          typename SectionIdTag::type local_section_id,
+                          TargetProxy local_target_proxy,
+                          ReductionData local_data)
+      : section_proxy(local_section_proxy),
+        section_id(local_section_id),
+        target_proxy(local_target_proxy),
+        data(local_data) {}
+  void pup(PUP::er& p) {
+    CMessage_SectionReductionMessage<SectionProxy, TargetProxy, ReductionData,
+                                     SectionIdTag>::pup(p);
+    p | section_proxy;
+    p | section_id;
+    p | target_proxy;
+    p | data;
+  }
+};
