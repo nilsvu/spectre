3f3c5faf39bd9de31e8c308c6944d79c628496e1
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-06-08 09:58:44 +0200

    WIP add multigrid

---
+++ src/ParallelAlgorithms/LinearSolver/Multigrid/Actions/RestrictFields.hpp
@@ -0,0 +1,179 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <cstddef>
+#include <map>
+
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "DataStructures/DataBox/PrefixHelpers.hpp"
+#include "DataStructures/FixedHashMap.hpp"
+#include "Domain/Structure/ElementId.hpp"
+#include "IO/Observer/Tags.hpp"
+#include "Informer/Tags.hpp"
+#include "NumericalAlgorithms/Convergence/Tags.hpp"
+#include "Parallel/GlobalCache.hpp"
+#include "Parallel/InboxInserters.hpp"
+#include "Parallel/Tags.hpp"
+#include "ParallelAlgorithms/LinearSolver/Multigrid/InterMeshOperators.hpp"
+#include "ParallelAlgorithms/LinearSolver/Multigrid/MeshHierarchy.hpp"
+#include "ParallelAlgorithms/LinearSolver/Multigrid/Tags.hpp"
+#include "ParallelAlgorithms/LinearSolver/Tags.hpp"
+#include "Utilities/ConstantExpressions.hpp"
+#include "Utilities/GetOutput.hpp"
+#include "Utilities/TaggedTuple.hpp"
+
+/// \cond
+template <size_t Dim>
+struct ElementId;
+/// \endcond
+
+namespace LinearSolver::multigrid {
+
+template <size_t Dim, typename ReceiveTag>
+struct DataFromChildrenInboxTag
+    : public Parallel::InboxInserters::Map<
+          DataFromChildrenInboxTag<Dim, ReceiveTag>> {
+  using temporal_id = size_t;
+  using type =
+      std::map<temporal_id, FixedHashMap<two_to_the(Dim), ElementId<Dim>,
+                                         typename ReceiveTag::type,
+                                         boost::hash<ElementId<Dim>>>>;
+};
+
+namespace Actions {
+
+template <typename FieldsTag, typename OptionsGroup, typename FieldIsMassiveTag,
+          typename ReceiveTag = FieldsTag>
+struct SendFieldsToCoarserGrid {
+  template <typename DbTagsList, typename... InboxTags, typename Metavariables,
+            size_t Dim, typename ActionList, typename ParallelComponent>
+  static std::tuple<db::DataBox<DbTagsList>&&> apply(
+      db::DataBox<DbTagsList>& box,
+      const tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
+      Parallel::GlobalCache<Metavariables>& cache,
+      const ElementId<Dim>& element_id, const ActionList /*meta*/,
+      const ParallelComponent* const /*meta*/) noexcept {
+    // Skip restriction on coarsest level
+    const auto& parent_id = get<Tags::ParentElementId<Dim>>(box);
+    if (not parent_id.has_value()) {
+      return {std::move(box)};
+    }
+
+    const auto& temporal_id =
+        db::get<Convergence::Tags::IterationId<OptionsGroup>>(box);
+    if (UNLIKELY(get<logging::Tags::Verbosity<OptionsGroup>>(box) >=
+                 ::Verbosity::Debug)) {
+      Parallel::printf("%s " + Options::name<OptionsGroup>() +
+                           "(%zu): Send fields to coarser grid\n",
+                       element_id, temporal_id);
+    }
+
+    // Restrict the fields to the coarser (parent) grid.
+    // We restrict before sending the data so the restriction operation is
+    // parellelized. The parent only needs to sum up all child contributions.
+    // TODO: Do nothing when parent is the same element
+    bool massive = false;
+    if constexpr (not std::is_same_v<FieldIsMassiveTag, void>) {
+      massive = db::get<FieldIsMassiveTag>(box);
+    }
+    const auto& mesh = db::get<domain::Tags::Mesh<Dim>>(box);
+    const auto& parent_mesh = db::get<Tags::ParentMesh<Dim>>(box);
+    const auto restriction_operator = multigrid::restriction_operator(
+        mesh, parent_mesh, element_id.segment_ids(), parent_id->segment_ids(),
+        massive);
+    auto restricted_fields = typename ReceiveTag::type(apply_matrices(
+        restriction_operator, db::get<FieldsTag>(box),
+        // TODO: make sure apply_matrices works for non-square matrices
+        mesh.extents()));
+
+    auto& receiver_proxy =
+        Parallel::get_parallel_component<ParallelComponent>(cache);
+    Parallel::receive_data<DataFromChildrenInboxTag<Dim, ReceiveTag>>(
+        receiver_proxy[*parent_id], temporal_id,
+        std::make_pair(element_id, std::move(restricted_fields)),
+        // We re-start the algorithm on coarser levels when they
+        // `receive_data`, since it is terminated in `PrepareSolve`.
+        true);
+    return {std::move(box)};
+  }
+};
+
+template <size_t Dim, typename FieldsTag, typename OptionsGroup,
+          typename ReceiveTag = FieldsTag>
+struct ReceiveFieldsFromFinerGrid {
+  using inbox_tags = tmpl::list<DataFromChildrenInboxTag<Dim, ReceiveTag>>;
+
+  template <typename DbTags, typename... InboxTags, typename Metavariables>
+  static bool is_ready(const db::DataBox<DbTags>& box,
+                       const tuples::TaggedTuple<InboxTags...>& inboxes,
+                       const Parallel::GlobalCache<Metavariables>& /*cache*/,
+                       const ElementId<Dim>& /*element_id*/) noexcept {
+    const auto& child_ids = get<Tags::ChildElementIds<Dim>>(box);
+    if (child_ids.empty()) {
+      return true;
+    }
+    const auto& inbox =
+        tuples::get<DataFromChildrenInboxTag<Dim, ReceiveTag>>(inboxes);
+    const auto& temporal_id =
+        db::get<Convergence::Tags::IterationId<OptionsGroup>>(box);
+    const auto temporal_received = inbox.find(temporal_id);
+    if (temporal_received == inbox.end()) {
+      return false;
+    }
+    const auto& received_children_data = temporal_received->second;
+    for (const auto& child_id : child_ids) {
+      if (received_children_data.find(child_id) ==
+          received_children_data.end()) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  template <typename DbTagsList, typename... InboxTags, typename Metavariables,
+            typename ActionList, typename ParallelComponent>
+  static std::tuple<db::DataBox<DbTagsList>&&> apply(
+      db::DataBox<DbTagsList>& box, tuples::TaggedTuple<InboxTags...>& inboxes,
+      const Parallel::GlobalCache<Metavariables>& /*cache*/,
+      const ElementId<Dim>& element_id, const ActionList /*meta*/,
+      const ParallelComponent* const /*meta*/) noexcept {
+    // Skip on finest grid
+    const auto& child_ids = get<Tags::ChildElementIds<Dim>>(box);
+    if (child_ids.empty()) {
+      return {std::move(box)};
+    }
+
+    const auto& temporal_id =
+        db::get<Convergence::Tags::IterationId<OptionsGroup>>(box);
+    if (UNLIKELY(get<logging::Tags::Verbosity<OptionsGroup>>(box) >=
+                 ::Verbosity::Debug)) {
+      Parallel::printf("%s " + Options::name<OptionsGroup>() +
+                           "(%zu): Receive fields from finer grid\n",
+                       element_id, temporal_id);
+    }
+
+    auto children_data = std::move(
+        tuples::get<DataFromChildrenInboxTag<Dim, ReceiveTag>>(inboxes)
+            .extract(temporal_id)
+            .mapped());
+
+    // Assemble restricted data from children
+    // TODO: Specialize for when single child is the same element
+    db::mutate<ReceiveTag>(
+        make_not_null(&box),
+        [&children_data](const auto source, const Mesh<Dim>& mesh) noexcept {
+          *source = typename ReceiveTag::type{mesh.number_of_grid_points(), 0.};
+          for (auto& child_id_and_data : children_data) {
+            *source += child_id_and_data.second;
+          }
+        },
+        db::get<domain::Tags::Mesh<Dim>>(box));
+
+    return {std::move(box)};
+  }
+};
+
+}  // namespace Actions
+}  // namespace LinearSolver::multigrid
---
+++ tests/Unit/Helpers/ParallelAlgorithms/LinearSolver/Multigrid/Helpers.hpp
@@ -0,0 +1,185 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <cstddef>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "DataStructures/DataBox/PrefixHelpers.hpp"
+#include "DataStructures/DataBox/Tag.hpp"
+#include "DataStructures/DenseMatrix.hpp"
+#include "DataStructures/Variables.hpp"
+#include "Domain/Structure/ElementId.hpp"
+#include "Domain/Tags.hpp"
+#include "Helpers/ParallelAlgorithms/LinearSolver/DistributedLinearSolverAlgorithmTestHelpers.hpp"
+#include "Options/Options.hpp"
+#include "Parallel/GlobalCache.hpp"
+#include "Parallel/Invoke.hpp"
+#include "Parallel/Reduction.hpp"
+#include "Parallel/Tags.hpp"
+#include "ParallelAlgorithms/Initialization/MutateAssign.hpp"
+#include "ParallelAlgorithms/LinearSolver/Multigrid/Tags.hpp"
+#include "ParallelAlgorithms/LinearSolver/Tags.hpp"
+#include "Utilities/Blas.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/TMPL.hpp"
+#include "Utilities/TaggedTuple.hpp"
+
+namespace helpers_distributed = DistributedLinearSolverAlgorithmTestHelpers;
+
+namespace TestHelpers::LinearSolver::multigrid {
+
+namespace OptionTags {
+struct LinearOperator {
+  static constexpr Options::String help = "The linear operator A to invert.";
+  using type =
+      std::vector<std::vector<DenseMatrix<double, blaze::columnMajor>>>;
+};
+}  // namespace OptionTags
+
+struct LinearOperator : db::SimpleTag {
+  using type =
+      std::vector<std::vector<DenseMatrix<double, blaze::columnMajor>>>;
+  using option_tags = tmpl::list<OptionTags::LinearOperator>;
+
+  static constexpr bool pass_metavariables = false;
+  static type create_from_options(const type& linear_operator) noexcept {
+    return linear_operator;
+  }
+};
+
+using fields_tag = helpers_distributed::fields_tag;
+using sources_tag = helpers_distributed::sources_tag;
+
+struct InitializeElement {
+  using const_global_cache_tags = tmpl::list<helpers_distributed::Source>;
+  using simple_tags = tmpl::list<fields_tag, sources_tag>;
+  using compute_tags = tmpl::list<>;
+
+  template <typename DbTagsList, typename... InboxTags, typename Metavariables,
+            typename ActionList, typename ParallelComponent>
+  static std::tuple<db::DataBox<DbTagsList>&&> apply(
+      db::DataBox<DbTagsList>& box,
+      const tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
+      const Parallel::GlobalCache<Metavariables>& /*cache*/,
+      const ElementId<1>& element_id, const ActionList /*meta*/,
+      const ParallelComponent* const /*meta*/) noexcept {
+    const size_t element_index = helpers_distributed::get_index(element_id);
+    const int mg_lev = element_id.segment_ids()[0].refinement_level();
+    const auto& source =
+        mg_lev == 1 ? typename sources_tag::type(gsl::at(
+                          get<helpers_distributed::Source>(box), element_index))
+                    : typename sources_tag::type{};
+    const size_t num_points =
+        db::get<::domain::Tags::Mesh<1>>(box).number_of_grid_points();
+    auto initial_fields = typename fields_tag::type{num_points, 0.};
+
+    Initialization::mutate_assign<simple_tags>(
+        make_not_null(&box), std::move(initial_fields), std::move(source));
+    return {std::move(box)};
+  }
+};
+
+template <typename OperandTag>
+struct CollectOperatorAction;
+
+template <typename OperandTag>
+struct ComputeOperatorAction {
+  using const_global_cache_tags = tmpl::list<LinearOperator>;
+  using local_operator_applied_to_operand_tag =
+      db::add_tag_prefix<::LinearSolver::Tags::OperatorAppliedTo, OperandTag>;
+
+  template <typename DbTagsList, typename... InboxTags, typename Metavariables,
+            typename ActionList, typename ParallelComponent>
+  static std::tuple<db::DataBox<DbTagsList>&&, bool> apply(
+      db::DataBox<DbTagsList>& box,
+      const tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
+      const Parallel::GlobalCache<Metavariables>& /*cache*/,
+      const ElementId<1>& element_id, const ActionList /*meta*/,
+      const ParallelComponent* const /*meta*/) noexcept {
+    const size_t mg_lev =
+        db::get<::LinearSolver::multigrid::Tags::MultigridLevel>(box);
+    const size_t element_index = helpers_distributed::get_index(element_id);
+    const auto& operator_matrices = get<LinearOperator>(box)[mg_lev];
+    const auto number_of_elements = operator_matrices.size();
+    const auto& linear_operator = gsl::at(operator_matrices, element_index);
+    const auto number_of_grid_points = linear_operator.columns();
+    const auto& operand = get<OperandTag>(box);
+
+    typename OperandTag::type operator_applied_to_operand{
+        number_of_grid_points * number_of_elements};
+    dgemv_('N', linear_operator.rows(), linear_operator.columns(), 1,
+           linear_operator.data(), linear_operator.spacing(), operand.data(), 1,
+           0, operator_applied_to_operand.data(), 1);
+
+    auto& section = *db::get<Parallel::Tags::SectionBase<
+        ::LinearSolver::multigrid::Tags::MultigridLevel>>(box);
+    Parallel::contribute_to_reduction<
+        CollectOperatorAction<OperandTag>, ParallelComponent,
+        ::LinearSolver::multigrid::Tags::MultigridLevel>(
+        Parallel::ReductionData<
+            Parallel::ReductionDatum<typename OperandTag::type, funcl::Plus<>>,
+            Parallel::ReductionDatum<size_t, funcl::AssertEqual<>>>{
+            operator_applied_to_operand, mg_lev},
+        section[element_id], section, section, mg_lev);
+
+    // Terminate algorithm for now. The reduction will be broadcast to the
+    // next action which is responsible for restarting the algorithm.
+    return {std::move(box), true};
+  }
+};
+
+template <typename OperandTag>
+struct CollectOperatorAction {
+  using local_operator_applied_to_operand_tag =
+      db::add_tag_prefix<::LinearSolver::Tags::OperatorAppliedTo, OperandTag>;
+
+  template <typename ParallelComponent, typename DbTagsList,
+            typename Metavariables, typename ScalarFieldOperandTag,
+            Requires<tmpl::list_contains_v<
+                DbTagsList, local_operator_applied_to_operand_tag>> = nullptr>
+  static void apply(
+      db::DataBox<DbTagsList>& box, Parallel::GlobalCache<Metavariables>& cache,
+      const ElementId<1>& element_id,
+      const Variables<tmpl::list<ScalarFieldOperandTag>>& Ap_global_data,
+      const size_t broadcasting_mg_lev) noexcept {
+    // FIXME: We're receiving broadcasts also from reductions over other
+    // sections for some reason
+    const size_t mg_lev =
+        db::get<::LinearSolver::multigrid::Tags::MultigridLevel>(box);
+    if (mg_lev != broadcasting_mg_lev) {
+      // Parallel::printf("Received broadcast on other MG level, ignoring.\n");
+      return;
+    }
+    const size_t element_index = helpers_distributed::get_index(element_id);
+    // This could be generalized to work on the Variables instead of the
+    // Scalar, but it's only for the purpose of this test.
+    const auto number_of_grid_points =
+        get<LinearOperator>(box)[mg_lev][0].columns();
+    const auto& Ap_global = get<ScalarFieldOperandTag>(Ap_global_data).get();
+    DataVector Ap_local{number_of_grid_points};
+    std::copy(Ap_global.begin() +
+                  static_cast<int>(element_index * number_of_grid_points),
+              Ap_global.begin() +
+                  static_cast<int>((element_index + 1) * number_of_grid_points),
+              Ap_local.begin());
+    db::mutate<local_operator_applied_to_operand_tag>(
+        make_not_null(&box),
+        [&Ap_local, &number_of_grid_points](auto Ap) noexcept {
+          *Ap = typename local_operator_applied_to_operand_tag::type{
+              number_of_grid_points};
+          get(get<
+              ::LinearSolver::Tags::OperatorAppliedTo<ScalarFieldOperandTag>>(
+              *Ap)) = Ap_local;
+        });
+    // Proceed with algorithm
+    Parallel::get_parallel_component<ParallelComponent>(cache)[element_id]
+        .perform_algorithm(true);
+  }
+};
+
+}  // namespace TestHelpers::LinearSolver::multigrid
