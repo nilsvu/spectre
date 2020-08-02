199629a78b32d53e07487f2243427c08a4edd091
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-08-02 18:49:58 +0200

    Add elliptic initial guess tags

--- src/Elliptic/Tags.hpp
+++ src/Elliptic/Tags.hpp
@@ -10,6 +10,27 @@
 
 /// Functionality related to solving elliptic partial differential equations
 namespace elliptic {
+
+namespace OptionTags {
+
+struct InitialGuessGroup {
+  static std::string name() noexcept { return "InitialGuess"; }
+  static constexpr Options::String help =
+      "Initial guess for the elliptic solve";
+};
+
+template <typename InitialGuessType>
+struct InitialGuess {
+  static std::string name() noexcept {
+    return Options::name<InitialGuessType>();
+  }
+  static constexpr Options::String help = "Options for the initial guess";
+  using group = InitialGuessGroup;
+  using type = InitialGuessType;
+};
+
+}  // namespace OptionTags
+
 namespace Tags {
 
 /*!
@@ -35,6 +56,15 @@ struct FluxesComputer : db::SimpleTag {
   static FluxesComputerType create_from_options() {
     return FluxesComputerType{};
   }
+};
+
+template <typename InitialGuessType>
+struct InitialGuess : db::SimpleTag {
+  using type = InitialGuessType;
+  using option_tags = tmpl::list<OptionTags::InitialGuess<InitialGuessType>>;
+
+  static constexpr bool pass_metavariables = false;
+  static type create_from_options(const type& value) noexcept { return value; }
 };
 
 }  // namespace Tags
