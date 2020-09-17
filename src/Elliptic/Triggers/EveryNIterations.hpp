df925de231150e6d00e0c8229f015082f71fb72f
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-09-17 16:36:10 +0200

    WIP obs events

--- src/Elliptic/Triggers/EveryNIterations.hpp
+++ src/Elliptic/Triggers/EveryNIterations.hpp
@@ -8,6 +8,7 @@
 
 #include "Options/Options.hpp"
 #include "Parallel/CharmPupable.hpp"
+#include "Parallel/Tags.hpp"
 #include "ParallelAlgorithms/EventsAndTriggers/Trigger.hpp"
 #include "Utilities/Registration.hpp"
 #include "Utilities/TMPL.hpp"
@@ -15,21 +16,42 @@
 namespace elliptic {
 namespace Triggers {
 /// \cond
-template <typename IterationId, typename TriggerRegistrars>
+template <typename IterationId, typename ArraySectionIdTag,
+          typename TriggerRegistrars>
 class EveryNIterations;
 /// \endcond
 
 namespace Registrars {
-template <typename IterationId>
+template <typename IterationId, typename ArraySectionIdTag = void>
 using EveryNIterations =
-    ::Registration::Registrar<Triggers::EveryNIterations, IterationId>;
+    ::Registration::Registrar<Triggers::EveryNIterations, IterationId,
+                              ArraySectionIdTag>;
 }  // namespace Registrars
+
+namespace detail {
+template <typename LocalArraySectionIdTag>
+struct ArraySectionIdOption {
+  static std::string name() noexcept {
+    return Options::name<LocalArraySectionIdTag>();
+  }
+  using type = typename LocalArraySectionIdTag::type;
+  static constexpr Options::String help{"Array section to trigger on."};
+  static type default_value() noexcept {
+    return std::numeric_limits<type>::max();
+  }
+};
+
+template <>
+struct ArraySectionIdOption<void> {
+  using type = void;
+};
+}  // namespace detail
 
 /// \ingroup EventsAndTriggersGroup
 /// Trigger every N iterations after a given offset.
-template <typename IterationId,
-          typename TriggerRegistrars =
-              tmpl::list<Registrars::EveryNIterations<IterationId>>>
+template <typename IterationId, typename ArraySectionIdTag = void,
+          typename TriggerRegistrars = tmpl::list<
+              Registrars::EveryNIterations<IterationId, ArraySectionIdTag>>>
 class EveryNIterations : public Trigger<TriggerRegistrars> {
  public:
   /// \cond
@@ -38,6 +60,10 @@ class EveryNIterations : public Trigger<TriggerRegistrars> {
   using PUP::able::register_constructor;
   WRAPPED_PUPable_decl_template(EveryNIterations);  // NOLINT
   /// \endcond
+
+  using ArraySectionId = tmpl::conditional_t<
+      std::is_same_v<ArraySectionIdTag, void>, bool,
+      typename detail::ArraySectionIdOption<ArraySectionIdTag>::type>;
 
   struct N {
     using type = uint64_t;
@@ -49,36 +75,57 @@ class EveryNIterations : public Trigger<TriggerRegistrars> {
     static constexpr Options::String help{"First iteration to trigger on."};
   };
 
-  using options = tmpl::list<N, Offset>;
+  using options = tmpl::flatten<tmpl::list<
+      N, Offset,
+      tmpl::conditional_t<std::is_same_v<ArraySectionIdTag, void>, tmpl::list<>,
+                          detail::ArraySectionIdOption<ArraySectionIdTag>>>>;
   static constexpr Options::String help{
       "Trigger every N iterations after a given offset."};
 
   EveryNIterations(const uint64_t interval, const uint64_t offset) noexcept
       : interval_(interval), offset_(offset) {}
 
-  using argument_tags = tmpl::list<IterationId>;
+  EveryNIterations(const uint64_t interval, const uint64_t offset,
+                   ArraySectionId array_section_id) noexcept
+      : interval_(interval),
+        offset_(offset),
+        array_section_id_(std::move(array_section_id)) {}
 
-  bool operator()(const typename IterationId::type& iteration_id) const
-      noexcept {
+  using argument_tags = tmpl::flatten<tmpl::list<
+      IterationId, tmpl::conditional_t<std::is_same_v<ArraySectionIdTag, void>,
+                                       tmpl::list<>, ArraySectionIdTag>>>;
+
+  bool operator()(
+      const typename IterationId::type& iteration_id) const noexcept {
     const auto step_number = static_cast<uint64_t>(iteration_id);
     return step_number >= offset_ and (step_number - offset_) % interval_ == 0;
+  }
+
+  bool operator()(const typename IterationId::type& iteration_id,
+                  const ArraySectionId& array_section_id) const noexcept {
+    return (array_section_id_ == std::numeric_limits<ArraySectionId>::max() or
+            array_section_id == array_section_id_) and
+           (*this)(iteration_id);
   }
 
   // clang-tidy: google-runtime-references
   void pup(PUP::er& p) noexcept {  // NOLINT
     p | interval_;
     p | offset_;
+    p | array_section_id_;
   }
 
  private:
   uint64_t interval_{0};
   uint64_t offset_{0};
+  ArraySectionId array_section_id_ = std::numeric_limits<ArraySectionId>::max();
 };
 
 /// \cond
-template <typename IterationId, typename TriggerRegistrars>
-PUP::able::PUP_ID EveryNIterations<IterationId, TriggerRegistrars>::my_PUP_ID =
-    0;  // NOLINT
+template <typename IterationId, typename ArraySectionIdTag,
+          typename TriggerRegistrars>
+PUP::able::PUP_ID EveryNIterations<IterationId, ArraySectionIdTag,
+                                   TriggerRegistrars>::my_PUP_ID = 0;  // NOLINT
 /// \endcond
 }  // namespace Triggers
 }  // namespace elliptic
