d5aca074f928c6d5829a12be7a198cbca25994da
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-10-09 11:44:50 +0200

    Reformulate curved Poisson equation
    Move the term that contains Christoffel symbols to the sources, since
    it's the non-principal part of the equations.

--- tests/Unit/Elliptic/Systems/Poisson/Equations.py
+++ tests/Unit/Elliptic/Systems/Poisson/Equations.py
@@ -8,10 +8,12 @@ def euclidean_fluxes(field_gradient):
     return field_gradient
 
 
-def non_euclidean_fluxes(inv_spatial_metric, det_spatial_metric,
-                         field_gradient):
-    return np.sqrt(det_spatial_metric) * np.einsum('ij,j', inv_spatial_metric,
-                                                   field_gradient)
+def non_euclidean_fluxes(inv_spatial_metric, field_gradient):
+    return np.einsum('ij,j', inv_spatial_metric, field_gradient)
+
+
+def add_non_euclidean_sources(christoffel_contracted, field_flux):
+    return -np.einsum('i,i', christoffel_contracted, field_flux)
 
 
 def auxiliary_fluxes(field, dim):
