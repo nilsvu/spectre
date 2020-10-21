269dddb2bd402df3245be4f999264b342df022da
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-10-21 19:34:20 +0200

    Reproduce SIGBUS in merge_into_databox #2524
    This reproduces the issue on my machine:
    
    make -j12 ExportCoordinates3D \
      && ./bin/ExportCoordinates3D +p2 \
        --input-file tests/InputFiles/ExportCoordinates/Input3D.yaml

--- src/Executables/ExportCoordinates/ExportCoordinates.hpp
+++ src/Executables/ExportCoordinates/ExportCoordinates.hpp
@@ -111,6 +111,32 @@ struct ExportCoordinates {
     return std::forward_as_tuple(std::move(box));
   }
 };
+
+template <size_t Dim, size_t I>
+struct TensorTag : db::SimpleTag {
+  using type = tnsr::Ijj<DataVector, Dim>;
+};
+
+template <typename Is>
+struct InitializeManyTensors;
+
+template <size_t... Is>
+struct InitializeManyTensors<std::index_sequence<Is...>> {
+  template <typename DbTagsList, typename... InboxTags, typename Metavariables,
+            size_t Dim, typename ActionList, typename ParallelComponent>
+  static auto apply(db::DataBox<DbTagsList>& box,
+                    const tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
+                    const Parallel::GlobalCache<Metavariables>& /*cache*/,
+                    const ElementId<Dim>& /*array_index*/,
+                    const ActionList /*meta*/,
+                    const ParallelComponent* const /*meta*/) noexcept {
+    return std::make_tuple(
+        ::Initialization::merge_into_databox<
+            InitializeManyTensors, db::AddSimpleTags<TensorTag<Dim, Is>...>>(
+            std::move(box), typename TensorTag<Dim, Is>::type{}...));
+  }
+};
+
 }  // namespace Actions
 
 template <size_t Dim>
@@ -145,6 +171,8 @@ struct Metavariables {
                       Actions::SetupDataBox,
                       Initialization::Actions::TimeAndTimeStep<Metavariables>,
                       evolution::dg::Initialization::Domain<Dim>,
+                      Actions::InitializeManyTensors<
+                          std::make_index_sequence<300>>,
                       ::Initialization::Actions::
                           RemoveOptionsAndTerminatePhase>>,
               Parallel::PhaseActions<
