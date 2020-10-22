1b62960fd274f5717ec16dc54be0e027292de562
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-10-22 21:36:57 +0200

    Add Schwarz solver algorithm
    This Schwarz-type linear solver works by solving many sub-problems in
    parallel and combining their solutions as a weighted sum to converge towards
    the global solution. Each sub-problem is the restriction of the global
    problem to an element-centered subdomain, which consists of a central element
    and an overlap region with its neighbors. The decomposition into independent
    sub-problems makes this linear solver very parallelizable, avoiding global
    synchronization points altogether. It is commonly used as a preconditioner to
    Krylov-type solvers such as `LinearSolver::gmres::Gmres` or
    `LinearSolver::cg::ConjugateGradient`, or as the "smoother" for a Multigrid
    solver (which may in turn precondition a Krylov-type solver).

--- src/ParallelAlgorithms/LinearSolver/Schwarz/Schwarz.hpp
+++ src/ParallelAlgorithms/LinearSolver/Schwarz/Schwarz.hpp
@@ -3,5 +3,150 @@
 
 #pragma once
 
+#include "IO/Observer/Helpers.hpp"
+#include "ParallelAlgorithms/LinearSolver/AsynchronousSolvers/ElementActions.hpp"
+#include "ParallelAlgorithms/LinearSolver/Schwarz/ElementActions.hpp"
+#include "ParallelAlgorithms/LinearSolver/Schwarz/Protocols.hpp"
+#include "Utilities/ProtocolHelpers.hpp"
+#include "Utilities/TMPL.hpp"
+
 /// Items related to the %Schwarz linear solver
-namespace LinearSolver::Schwarz {}
+///
+/// \see `LinearSolver::Schwarz::Schwarz`
+namespace LinearSolver::Schwarz {
+
+/*!
+ * \ingroup LinearSolverGroup
+ * \brief An additive Schwarz subdomain solver for linear systems of equations
+ *
+ * This Schwarz-type linear solver works by solving many sub-problems in
+ * parallel and combining their solutions as a weighted sum to converge towards
+ * the global solution. Each sub-problem is the restriction of the global
+ * problem to an element-centered subdomain, which consists of a central element
+ * and an overlap region with its neighbors. The decomposition into independent
+ * sub-problems makes this linear solver very parallelizable, avoiding global
+ * synchronization points altogether. It is commonly used as a preconditioner to
+ * Krylov-type solvers such as `LinearSolver::gmres::Gmres` or
+ * `LinearSolver::cg::ConjugateGradient`, or as the "smoother" for a Multigrid
+ * solver (which may in turn precondition a Krylov-type solver).
+ *
+ * \par Subdomain geometry:
+ * The image below gives an overview of the structure of an element-centered
+ * subdomain:
+ *
+ * \image html subdomain_structure.svg "Fig. 1: An element-centered subdomain" width=600px
+ *
+ * Fig. 1 shows part of a 2-dimensional computational domain. The domain is
+ * composed of elements (light gray) that each carry a Legendre-Gauss-Lobatto
+ * mesh of grid points (black dots). The diagonally shaded region to the left
+ * illustrates an external domain boundary. The lines between neighboring
+ * element faces illustrate mortar meshes, which are relevant when implementing
+ * a subdomain operator in a DG context but play no role in the Schwarz
+ * algorithm. The dashed line gives an example of an element-centered subdomain
+ * with a maximum of 2 grid points of overlap into neighboring elements. For
+ * details on how the number of overlap points is chosen see
+ * `LinearSolver::Schwarz::overlap_extent`. Note that the subdomain does not
+ * extend into corner- or edge-neighbors, which is different to both
+ * \cite Stiller2016a and \cite Vincent2019qpd. The reason we don't include
+ * such diagonal couplings is that in a DG context information only propagates
+ * across faces, as noted in \cite Stiller2016a. By eliminating the corner-
+ * and edge-neighbors we significantly reduce the complexity of the subdomain
+ * geometry and potentially also the communication costs. However, the
+ * advantages and disadvantages of this choice have yet to be carefully
+ * evaluated.
+ *
+ * \par Subdomain operator:
+ * The Schwarz subdomain solver relies on a restriction of the global linear
+ * problem \f$Ax=b\f$ to the subdomains. The subdomain operator
+ * \f$A_{ss}=R_s A R_s^T\f$, where \f$R_s\f$ is the restriction operator,
+ * essentially assumes that its operand is zero everywhere but within the
+ * subdomain (see Section 3.1 in \cite Stiller2016a). See
+ * `LinearSolver::Schwarz::protocols::SubdomainOperator` for details on how to
+ * implement a subdomain operator for your problem.
+ *
+ * \par Algorithm overview:
+ * In each iteration, the Schwarz algorithm restricts the residual \f$r=b-Ax\f$
+ * to all subdomains and communicates data on overlap regions with neighboring
+ * elements. Once an element has received all data on its subdomain it solves
+ * the sub-problem \f$A_{ss}\delta x_s=r_s\f$ for the correction \f$\delta
+ * x_s\f$, where \f$\delta x_s\f$ and \f$r_s\f$ have data only on the points of
+ * the subdomain and \f$A_{ss}\f$ is the subdomain operator. Since all elements
+ * perform such a subdomain-solve, we end up with a subdomain solution \f$\delta
+ * x_s\f$ on every subdomain, and the solutions overlap. Therefore, the
+ * algorithm communicates subdomain solutions on overlap regions with
+ * neighboring elements and adds them to the solution field \f$x\f$ as a
+ * weighted sum.
+ *
+ * \par Subdomain solves:
+ * Each subdomain solve is local to an element, since the data on the subdomain
+ * has been made available through communication with the neighboring elements.
+ * We can now use any means to solve the subdomain problem that's appropriate
+ * for the subdomain operator \f$A_{ss}\f$. For example, if the subdomain
+ * operator was available as an explit matrix we could invert it directly. Since
+ * it is typically a matrix-free operator a common approach to solve the
+ * subdomain problem is by means of an iterative Krylov-type method, such as
+ * GMRES or Conjugate Gradient, ideally with an appropriate preconditioner (yes,
+ * this would be preconditioned Krylov-methods solving the subdomains of the
+ * Schwarz solver, which might in turn precondition a global Krylov-solver -
+ * it's preconditioners all the way down). Currently, we use
+ * `LinearSolver::Serial::Gmres` to solve subdomains. It supports
+ * preconditioning, and adding useful subdomain preconditioners will be the
+ * subject of future work. Note that the choice of subdomain solver (and, by
+ * extension, the choice of subdomain preconditioner) affects only the
+ * _performance_ of the Schwarz solver, not its convergence or parallelization
+ * properties (assuming the subdomain solutions it produces are sufficiently
+ * precise).
+ *
+ * \par Weighting:
+ * Once the subdomain solutions \f$\delta x_s\f$ have been found they must be
+ * combined where they have multiple values, i.e. on overlap regions of the
+ * subdomains. We use an additive approach where we combine the subdomain
+ * solutions as a weighted sum, which has the advantage over "multiplicative"
+ * Schwarz methods that the subdomains decouple and can be solved in parallel.
+ * See `LinearSolver::Schwarz::element_weight` and Section 3.1 of
+ * \cite Stiller2016a for details. Note that we must account for the missing
+ * corner- and edge-neighbors when constructing the weights. See
+ * `LinearSolver::Schwarz::intruding_weight` for a discussion.
+ *
+ * \par Possible improvements:
+ * - Specify the number of overlap points as a fraction of the element width
+ * instead of a fixed number. This was shown in \cite Stiller2016b to achieve
+ * scale-independent convergence at the cost of increasing subdomain sizes.
+ * - Subdomain preconditioning (see paragraph on subdomain solves above)
+ */
+template <typename FieldsTag, typename OptionsGroup, typename SubdomainOperator,
+          typename SourceTag =
+              db::add_tag_prefix<::Tags::FixedSource, FieldsTag>>
+struct Schwarz {
+  using operand_tag = FieldsTag;
+  using fields_tag = FieldsTag;
+  using source_tag = SourceTag;
+  using options_group = OptionsGroup;
+  static_assert(
+      tt::assert_conforms_to<SubdomainOperator, protocols::SubdomainOperator>);
+
+  using component_list = tmpl::list<>;
+  using observed_reduction_data_tags = observers::make_reduction_data_tags<
+      tmpl::list<async_solvers::reduction_data, detail::reduction_data>>;
+
+  using initialize_element = tmpl::list<
+      async_solvers::InitializeElement<FieldsTag, OptionsGroup, SourceTag>,
+      detail::InitializeElement<FieldsTag, OptionsGroup, SubdomainOperator>>;
+
+  using register_element = tmpl::list<
+      async_solvers::RegisterElement<FieldsTag, OptionsGroup, SourceTag>,
+      detail::RegisterElement<FieldsTag, OptionsGroup, SourceTag>>;
+
+  template <typename ApplyOperatorActions, typename Label = OptionsGroup>
+  using solve = tmpl::list<
+      async_solvers::PrepareSolve<FieldsTag, OptionsGroup, SourceTag, Label>,
+      detail::SendOverlapData<FieldsTag, OptionsGroup, SubdomainOperator>,
+      detail::ReceiveOverlapData<FieldsTag, OptionsGroup, SubdomainOperator>,
+      detail::SolveSubdomain<FieldsTag, OptionsGroup, SubdomainOperator>,
+      detail::ReceiveOverlapSolution<FieldsTag, OptionsGroup,
+                                     SubdomainOperator>,
+      ApplyOperatorActions,
+      async_solvers::CompleteStep<FieldsTag, OptionsGroup, SourceTag, Label>>;
+};
+
+}  // namespace LinearSolver::Schwarz
---
+++ tests/Unit/ParallelAlgorithms/LinearSolver/Schwarz/Test_SchwarzAlgorithm.cpp
@@ -0,0 +1,227 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#define CATCH_CONFIG_RUNNER
+
+#include <cstddef>
+#include <vector>
+
+#include "DataStructures/ApplyMatrices.hpp"
+#include "DataStructures/Matrix.hpp"
+#include "Domain/Creators/RegisterDerivedWithCharm.hpp"
+#include "Domain/LogicalCoordinates.hpp"
+#include "Domain/Structure/Element.hpp"
+#include "Domain/Structure/ElementId.hpp"
+#include "Domain/Tags.hpp"
+#include "Elliptic/DiscontinuousGalerkin/DgElementArray.hpp"
+#include "ErrorHandling/FloatingPointExceptions.hpp"
+#include "Helpers/ParallelAlgorithms/LinearSolver/DistributedLinearSolverAlgorithmTestHelpers.hpp"
+#include "Helpers/ParallelAlgorithms/LinearSolver/LinearSolverAlgorithmTestHelpers.hpp"
+#include "NumericalAlgorithms/Spectral/Mesh.hpp"
+#include "NumericalAlgorithms/Spectral/Spectral.hpp"
+#include "Parallel/GlobalCache.hpp"
+#include "Parallel/InitializationFunctions.hpp"
+#include "Parallel/Main.hpp"
+#include "ParallelAlgorithms/Initialization/Actions/RemoveOptionsAndTerminatePhase.hpp"
+#include "ParallelAlgorithms/LinearSolver/Schwarz/ElementCenteredSubdomainData.hpp"
+#include "ParallelAlgorithms/LinearSolver/Schwarz/Protocols.hpp"
+#include "ParallelAlgorithms/LinearSolver/Schwarz/Schwarz.hpp"
+#include "Utilities/MakeArray.hpp"
+#include "Utilities/ProtocolHelpers.hpp"
+#include "Utilities/TMPL.hpp"
+
+namespace helpers = LinearSolverAlgorithmTestHelpers;
+namespace helpers_distributed = DistributedLinearSolverAlgorithmTestHelpers;
+
+namespace {
+
+struct SchwarzSmoother {
+  static constexpr Options::String help =
+      "Options for the iterative Schwarz smoother";
+};
+
+// [subdomain_operator]
+// Applies the linear operator given explicitly in the input file to data on an
+// element-centered subdomain, using standard matrix multiplications.
+//
+// We assume all elements have the same extents so we can use the element's
+// intruding overlap extents instead of initializing extruding overlap extents.
+struct SubdomainElementOperator {
+  using argument_tags = tmpl::list<
+      helpers_distributed::LinearOperator, domain::Tags::Element<1>,
+      LinearSolver::Schwarz::Tags::IntrudingExtents<1, SchwarzSmoother>>;
+
+  template <typename SubdomainData, typename ResultData,
+            typename SubdomainOperator>
+  static void apply(
+      const db::item_type<helpers_distributed::LinearOperator>& linear_operator,
+      const Element<1>& element,
+      const std::array<size_t, 1>& all_intruding_extents,
+      const SubdomainData& subdomain_data,
+      const gsl::not_null<ResultData*> result,
+      const gsl::not_null<SubdomainOperator*> /*subdomain_operator*/) noexcept {
+    const size_t element_index = helpers_distributed::get_index(element.id());
+
+    // Get the operator matrix slice for the central element
+    const auto& operator_slice = gsl::at(linear_operator, element_index);
+    const size_t num_points = operator_slice.columns();
+    const Index<1> volume_extents{num_points};
+
+    // Apply contribution to the central element
+    const Matrix operator_matrix_element = blaze::submatrix(
+        operator_slice, element_index * num_points, 0, num_points, num_points);
+    apply_matrices(make_not_null(&result->element_data),
+                   make_array<1>(operator_matrix_element),
+                   subdomain_data.element_data, volume_extents);
+
+    // Apply contribution to the overlaps
+    for (const auto& [overlap_id, overlap_data] : subdomain_data.overlap_data) {
+      // Silence unused-variable warning on GCC 7
+      (void)overlap_data;
+      const auto& direction = overlap_id.first;
+      const size_t overlapped_element_index = direction.side() == Side::Lower
+                                                  ? (element_index - 1)
+                                                  : (element_index + 1);
+      const Matrix operator_matrix_overlap = blaze::submatrix(
+          operator_slice, overlapped_element_index * num_points, 0, num_points,
+          num_points);
+      const auto overlap_contribution_extended =
+          apply_matrices(make_array<1>(operator_matrix_overlap),
+                         subdomain_data.element_data, volume_extents);
+      const auto& overlap_extents =
+          gsl::at(all_intruding_extents, direction.dimension());
+      const auto direction_from_neighbor = direction.opposite();
+      LinearSolver::Schwarz::data_on_overlap(
+          make_not_null(&result->overlap_data[overlap_id]),
+          overlap_contribution_extended, volume_extents, overlap_extents,
+          direction_from_neighbor);
+    }
+  }
+};
+
+template <typename Directions>
+struct SubdomainFaceOperator;
+
+template <>
+struct SubdomainFaceOperator<domain::Tags::InternalDirections<1>> {
+  using argument_tags = tmpl::list<
+      helpers_distributed::LinearOperator, domain::Tags::Element<1>,
+      domain::Tags::Direction<1>,
+      LinearSolver::Schwarz::Tags::IntrudingExtents<1, SchwarzSmoother>>;
+  using volume_tags = tmpl::list<
+      helpers_distributed::LinearOperator, domain::Tags::Element<1>,
+      LinearSolver::Schwarz::Tags::IntrudingExtents<1, SchwarzSmoother>>;
+
+  template <typename SubdomainData, typename ResultData,
+            typename SubdomainOperator>
+  static void apply(
+      const db::item_type<helpers_distributed::LinearOperator>& linear_operator,
+      const Element<1>& element, const Direction<1>& direction,
+      const std::array<size_t, 1>& all_intruding_extents,
+      const SubdomainData& subdomain_data,
+      const gsl::not_null<ResultData*> result,
+      const gsl::not_null<SubdomainOperator*> /*subdomain_operator*/) noexcept {
+    const size_t element_index = helpers_distributed::get_index(element.id());
+    for (const auto& [overlap_id, overlap_data] : subdomain_data.overlap_data) {
+      const auto& local_direction = overlap_id.first;
+      // Apply the operator only to this face in this invocation
+      if (local_direction != direction) {
+        continue;
+      }
+      const auto direction_from_neighbor = direction.opposite();
+      const size_t overlapped_element_index = direction.side() == Side::Lower
+                                                  ? (element_index - 1)
+                                                  : (element_index + 1);
+      const auto& overlap_extents =
+          gsl::at(all_intruding_extents, direction.dimension());
+
+      // Get the operator matrix slice for the overlapped element
+      const auto& operator_slice =
+          gsl::at(linear_operator, overlapped_element_index);
+      const size_t num_points = operator_slice.columns();
+      const Index<1> volume_extents{num_points};
+
+      // Get the overlap data extended by zeros
+      const auto extended_overlap_data =
+          LinearSolver::Schwarz::extended_overlap_data(
+              overlap_data, volume_extents, overlap_extents,
+              direction_from_neighbor);
+
+      // Apply contribution to the central element
+      const Matrix operator_matrix_element =
+          blaze::submatrix(operator_slice, element_index * num_points, 0,
+                           num_points, num_points);
+      result->element_data +=
+          apply_matrices(make_array<1>(operator_matrix_element),
+                         extended_overlap_data, volume_extents);
+
+      // Apply contribution to the overlap
+      const Matrix operator_matrix_overlap = blaze::submatrix(
+          operator_slice, overlapped_element_index * num_points, 0, num_points,
+          num_points);
+      const auto overlap_contribution_extended =
+          apply_matrices(make_array<1>(operator_matrix_overlap),
+                         extended_overlap_data, volume_extents);
+      result->overlap_data.at(overlap_id) +=
+          LinearSolver::Schwarz::data_on_overlap(
+              overlap_contribution_extended, volume_extents, overlap_extents,
+              direction_from_neighbor);
+    }
+  }
+};
+
+template <>
+struct SubdomainFaceOperator<domain::Tags::BoundaryDirectionsInterior<1>> {
+  using argument_tags = tmpl::list<>;
+  template <typename SubdomainData, typename ResultData,
+            typename SubdomainOperator>
+  static void apply(
+      const SubdomainData& /*subdomain_data*/,
+      const gsl::not_null<ResultData*> /*result*/,
+      const gsl::not_null<SubdomainOperator*> /*subdomain_operator*/) noexcept {
+  }
+};
+
+struct SubdomainOperator
+    : tt::ConformsTo<LinearSolver::Schwarz::protocols::SubdomainOperator> {
+  static constexpr size_t volume_dim = 1;
+  using element_operator = SubdomainElementOperator;
+  template <typename Directions>
+  using face_operator = SubdomainFaceOperator<Directions>;
+  explicit SubdomainOperator(const size_t /*element_num_points*/) noexcept {}
+};
+// [subdomain_operator]
+
+static_assert(tt::assert_conforms_to<
+              SubdomainOperator,
+              LinearSolver::Schwarz::protocols::SubdomainOperator>);
+
+struct Metavariables {
+  static constexpr const char* const help{
+      "Test the Schwarz linear solver algorithm"};
+  static constexpr size_t volume_dim = 1;
+
+  using linear_solver =
+      LinearSolver::Schwarz::Schwarz<helpers_distributed::fields_tag,
+                                     SchwarzSmoother, SubdomainOperator>;
+  using preconditioner = void;
+
+  using Phase = helpers::Phase;
+  using observed_reduction_data_tags =
+      helpers::observed_reduction_data_tags<Metavariables>;
+  using component_list = helpers_distributed::component_list<Metavariables>;
+  static constexpr bool ignore_unrecognized_command_line_options = false;
+  static constexpr auto determine_next_phase =
+      helpers::determine_next_phase<Metavariables>;
+};
+
+}  // namespace
+
+static const std::vector<void (*)()> charm_init_node_funcs{
+    &setup_error_handling, &domain::creators::register_derived_with_charm};
+static const std::vector<void (*)()> charm_init_proc_funcs{
+    &enable_floating_point_exceptions};
+
+using charmxx_main_component = Parallel::Main<Metavariables>;
+
+#include "Parallel/CharmMain.tpp"  // IWYU pragma: keep
