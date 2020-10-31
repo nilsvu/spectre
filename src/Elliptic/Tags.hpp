5681a12c06c64750e0da78013c40a30d2cfc1e5f
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-10-31 16:32:52 +0100

    Add elliptic background and inital guess tags

--- src/Elliptic/Tags.hpp
+++ src/Elliptic/Tags.hpp
@@ -6,10 +6,39 @@
 #include <string>
 
 #include "DataStructures/DataBox/Tag.hpp"
+#include "Options/Options.hpp"
 #include "Utilities/PrettyType.hpp"
 
 /// Functionality related to solving elliptic partial differential equations
 namespace elliptic {
+
+namespace OptionTags {
+
+template <typename BackgroundType>
+struct Background {
+  static std::string name() noexcept { return Options::name<BackgroundType>(); }
+  static constexpr Options::String help = "Options for the background";
+  using type = BackgroundType;
+};
+
+struct InitialGuessGroup {
+  static std::string name() noexcept { return "InitialGuess"; }
+  static constexpr Options::String help =
+      "Initial guess for the elliptic solve";
+};
+
+template <typename InitialGuessType>
+struct InitialGuess {
+  static std::string name() noexcept {
+    return Options::name<InitialGuessType>();
+  }
+  static constexpr Options::String help = "Options for the initial guess";
+  using group = InitialGuessGroup;
+  using type = InitialGuessType;
+};
+
+}  // namespace OptionTags
+
 namespace Tags {
 
 /*!
@@ -35,6 +64,28 @@ struct FluxesComputer : db::SimpleTag {
   static FluxesComputerType create_from_options() {
     return FluxesComputerType{};
   }
+};
+
+/// The variable-independent part of the elliptic equations, e.g. the
+/// fixed-sources \f$f(x)\f$ in a Poisson equation \f$-\Delta u=f(x)\f$, the
+/// matter-density in a TOV-solve or the conformal metric in an XCTS solve.
+template <typename BackgroundType>
+struct Background : db::SimpleTag {
+  using type = BackgroundType;
+  using option_tags = tmpl::list<OptionTags::Background<BackgroundType>>;
+
+  static constexpr bool pass_metavariables = false;
+  static type create_from_options(const type& value) noexcept { return value; }
+};
+
+/// The initial guess for the elliptic solve.
+template <typename InitialGuessType>
+struct InitialGuess : db::SimpleTag {
+  using type = InitialGuessType;
+  using option_tags = tmpl::list<OptionTags::InitialGuess<InitialGuessType>>;
+
+  static constexpr bool pass_metavariables = false;
+  static type create_from_options(const type& value) noexcept { return value; }
 };
 
 }  // namespace Tags
