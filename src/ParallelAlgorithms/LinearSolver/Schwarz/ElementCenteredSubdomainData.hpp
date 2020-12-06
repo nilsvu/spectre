6f3cf0898fd7acad6e37be11f6fc08fb2ff62fb1
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-12-06 14:39:28 +0100

    Provide data-access in ElementCenteredSubdomainData

--- src/ParallelAlgorithms/LinearSolver/Schwarz/ElementCenteredSubdomainData.hpp
+++ src/ParallelAlgorithms/LinearSolver/Schwarz/ElementCenteredSubdomainData.hpp
@@ -13,8 +13,12 @@
 #include "NumericalAlgorithms/LinearSolver/InnerProduct.hpp"
 #include "ParallelAlgorithms/LinearSolver/Schwarz/OverlapHelpers.hpp"
 #include "Utilities/MakeWithValue.hpp"
+#include "Utilities/TMPL.hpp"
 
 namespace LinearSolver::Schwarz {
+
+template <bool Const, size_t Dim, typename TagsList>
+struct ElementCenteredSubdomainDataIterator;
 
 /*!
  * \brief Data on an element-centered subdomain
@@ -50,6 +54,32 @@ struct ElementCenteredSubdomainData {
       OverlapMap<Dim, Variables<TagsList>> local_overlap_data) noexcept
       : element_data{std::move(local_element_data)},
         overlap_data{std::move(local_overlap_data)} {}
+
+  size_t size() const noexcept {
+    return std::accumulate(
+        overlap_data.begin(), overlap_data.end(), element_data.size(),
+        [](const size_t size, const auto& overlap_id_and_data) noexcept {
+          return size + overlap_id_and_data.second.size();
+        });
+  }
+
+  ElementCenteredSubdomainDataIterator<false, Dim, TagsList> begin() noexcept {
+    return {this};
+  }
+
+  ElementCenteredSubdomainDataIterator<false, Dim, TagsList> end() noexcept {
+    return {};
+  }
+
+  ElementCenteredSubdomainDataIterator<true, Dim, TagsList> begin() const
+      noexcept {
+    return {this};
+  }
+
+  ElementCenteredSubdomainDataIterator<true, Dim, TagsList> end() const
+      noexcept {
+    return {};
+  }
 
   void pup(PUP::er& p) noexcept {  // NOLINT
     p | element_data;
@@ -162,6 +192,116 @@ bool operator!=(
     const ElementCenteredSubdomainData<Dim, TagsList>& rhs) noexcept {
   return not(lhs == rhs);
 }
+
+template <size_t Dim, typename ValueType>
+std::vector<OverlapId<Dim>> ordered_overlap_ids(
+    const OverlapMap<Dim, ValueType>& overlap_map) noexcept {
+  std::vector<OverlapId<Dim>> overlap_ids{};
+  overlap_ids.reserve(overlap_map.size());
+  std::transform(overlap_map.begin(), overlap_map.end(),
+                 std::back_inserter(overlap_ids),
+                 [](const auto& overlap_id_and_value) noexcept {
+                   return overlap_id_and_value.first;
+                 });
+  std::sort(overlap_ids.begin(), overlap_ids.end(),
+            [](const OverlapId<Dim>& lhs, const OverlapId<Dim>& rhs) noexcept {
+              if (lhs.first.axis() != rhs.first.axis()) {
+                return lhs.first.axis() < rhs.first.axis();
+              }
+              if (lhs.first.side() != rhs.first.side()) {
+                return lhs.first.side() < rhs.first.side();
+              }
+              if (lhs.second.block_id() != rhs.second.block_id()) {
+                return lhs.second.block_id() < rhs.second.block_id();
+              }
+              for (size_t d = 0; d < Dim; ++d) {
+                const auto& lhs_segment_id = lhs.second.segment_ids().at(d);
+                const auto& rhs_segment_id = rhs.second.segment_ids().at(d);
+                if (lhs_segment_id.refinement_level() !=
+                    rhs_segment_id.refinement_level()) {
+                  return lhs_segment_id.refinement_level() <
+                         rhs_segment_id.refinement_level();
+                }
+                if (lhs_segment_id.index() != rhs_segment_id.index()) {
+                  return lhs_segment_id.index() < rhs_segment_id.index();
+                }
+              }
+              return false;
+            });
+  return overlap_ids;
+}
+
+/// If you need this for anything performance-critical you should do the
+/// work of implementing non-owning variables
+template <bool Const, size_t Dim, typename TagsList>
+struct ElementCenteredSubdomainDataIterator {
+ private:
+  using PtrType =
+      tmpl::conditional_t<Const,
+                          const ElementCenteredSubdomainData<Dim, TagsList>*,
+                          ElementCenteredSubdomainData<Dim, TagsList>*>;
+
+ public:
+  /// Construct begin state
+  ElementCenteredSubdomainDataIterator(PtrType data) noexcept : data_(data) {
+    overlap_ids_ = ordered_overlap_ids(data_->overlap_data);
+    reset();
+  }
+
+  void reset() noexcept {
+    overlap_index_ = (data_->element_data.size() == 0 and overlap_ids_.empty())
+                         ? std::numeric_limits<size_t>::max()
+                         : 0;
+    data_index_ = 0;
+  }
+
+  /// Construct end state
+  ElementCenteredSubdomainDataIterator() noexcept {
+    overlap_index_ = std::numeric_limits<size_t>::max();
+    data_index_ = 0;
+  }
+
+  ElementCenteredSubdomainDataIterator& operator++() noexcept {
+    ++data_index_;
+    if (data_index_ ==
+        (overlap_index_ == 0
+             ? data_->element_data
+             : data_->overlap_data.at(overlap_ids_[overlap_index_ - 1]))
+            .size()) {
+      ++overlap_index_;
+      data_index_ = 0;
+    }
+    if (overlap_index_ == overlap_ids_.size() + 1) {
+      overlap_index_ = std::numeric_limits<size_t>::max();
+    }
+    return *this;
+  }
+
+  tmpl::conditional_t<Const, double, double&> operator*() const noexcept {
+    if (overlap_index_ == 0) {
+      return data_->element_data.data()[data_index_];
+    } else {
+      return data_->overlap_data.at(overlap_ids_[overlap_index_ - 1])
+          .data()[data_index_];
+    }
+  }
+
+ private:
+  template <bool LocalConst, size_t LocalDim, typename LocalTagsList>
+  friend bool operator!=(
+      const ElementCenteredSubdomainDataIterator<LocalConst, LocalDim,
+                                                 LocalTagsList>& lhs,
+      const ElementCenteredSubdomainDataIterator<LocalConst, LocalDim,
+                                                 LocalTagsList>& rhs) noexcept {
+    return lhs.overlap_index_ != rhs.overlap_index_ or
+           lhs.data_index_ != rhs.data_index_;
+  }
+
+  PtrType data_;
+  std::vector<OverlapId<Dim>> overlap_ids_;
+  size_t overlap_index_;
+  size_t data_index_;
+};
 
 }  // namespace LinearSolver::Schwarz
 
