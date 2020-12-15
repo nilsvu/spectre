36ad475f9f2ab9d6480618cf91c2877e914b5dc1
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-12-15 18:52:06 +0100

    fixup Elliptic tags

--- src/Elliptic/Tags.hpp
+++ src/Elliptic/Tags.hpp
@@ -3,10 +3,12 @@
 
 #pragma once
 
+#include <memory>
 #include <string>
 
 #include "DataStructures/DataBox/Tag.hpp"
 #include "Options/Options.hpp"
+#include "Parallel/Serialize.hpp"
 #include "Utilities/PrettyType.hpp"
 
 /// Functionality related to solving elliptic partial differential equations
@@ -16,25 +18,19 @@ namespace OptionTags {
 
 template <typename BackgroundType>
 struct Background {
-  static std::string name() noexcept { return Options::name<BackgroundType>(); }
-  static constexpr Options::String help = "Options for the background";
-  using type = BackgroundType;
-};
-
-struct InitialGuessGroup {
-  static std::string name() noexcept { return "InitialGuess"; }
   static constexpr Options::String help =
-      "Initial guess for the elliptic solve";
+      "The variable-independent part of the equations that define the problem "
+      "to solve (along with the boundary conditions).";
+  using type = std::unique_ptr<BackgroundType>;
 };
 
 template <typename InitialGuessType>
 struct InitialGuess {
-  static std::string name() noexcept {
-    return Options::name<InitialGuessType>();
-  }
-  static constexpr Options::String help = "Options for the initial guess";
-  using group = InitialGuessGroup;
-  using type = InitialGuessType;
+  static constexpr Options::String help =
+      "The initial guess for the elliptic solve. The solve converges faster if "
+      "the initial guess is close to the solution and may not converge at all "
+      "if the initial guess is too far away.";
+  using type = std::unique_ptr<InitialGuessType>;
 };
 
 }  // namespace OptionTags
@@ -71,21 +67,25 @@ struct FluxesComputer : db::SimpleTag {
 /// matter-density in a TOV-solve or the conformal metric in an XCTS solve.
 template <typename BackgroundType>
 struct Background : db::SimpleTag {
-  using type = BackgroundType;
+  using type = std::unique_ptr<BackgroundType>;
   using option_tags = tmpl::list<OptionTags::Background<BackgroundType>>;
 
   static constexpr bool pass_metavariables = false;
-  static type create_from_options(const type& value) noexcept { return value; }
+  static type create_from_options(const type& value) noexcept {
+    return deserialize<type>(serialize<type>(value).data());
+  }
 };
 
 /// The initial guess for the elliptic solve.
 template <typename InitialGuessType>
 struct InitialGuess : db::SimpleTag {
-  using type = InitialGuessType;
+  using type = std::unique_ptr<InitialGuessType>;
   using option_tags = tmpl::list<OptionTags::InitialGuess<InitialGuessType>>;
 
   static constexpr bool pass_metavariables = false;
-  static type create_from_options(const type& value) noexcept { return value; }
+  static type create_from_options(const type& value) noexcept {
+    return deserialize<type>(serialize<type>(value).data());
+  }
 };
 
 }  // namespace Tags
