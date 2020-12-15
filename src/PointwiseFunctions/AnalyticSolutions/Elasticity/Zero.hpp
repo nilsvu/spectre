f33acf8af4cfa3b6ca8ad020a490d0c16675f35b
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-12-15 18:48:32 +0100

    fixup Upgrade Elasticity analytic solutions

--- src/PointwiseFunctions/AnalyticSolutions/Elasticity/Zero.hpp
+++ src/PointwiseFunctions/AnalyticSolutions/Elasticity/Zero.hpp
@@ -10,6 +10,7 @@
 #include "Elliptic/Protocols.hpp"
 #include "Elliptic/Systems/Elasticity/Tags.hpp"
 #include "Options/Options.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/Elasticity/AnalyticSolution.hpp"
 #include "Utilities/MakeWithValue.hpp"
 #include "Utilities/ProtocolHelpers.hpp"
 #include "Utilities/TMPL.hpp"
@@ -23,14 +24,24 @@ class er;
 /// \endcond
 
 namespace Elasticity::Solutions {
-template <size_t Dim>
-class Zero : public tt::ConformsTo<elliptic::protocols::AnalyticSolution> {
- public:
-  static constexpr size_t volume_dim = Dim;
-  using supported_tags =
-      tmpl::list<Tags::Displacement<Dim>, Tags::Strain<Dim>,
-                 ::Tags::FixedSource<Tags::Displacement<Dim>>>;
 
+/// \cond
+template <size_t Dim, typename Registrars>
+struct Zero;
+
+namespace Registrars {
+template <size_t Dim>
+struct Zero {
+  template <typename Registrars>
+  using f = Solutions::Zero<Dim, Registrars>;
+};
+}  // namespace Registrars
+/// \endcond
+
+template <size_t Dim,
+          typename Registrars = tmpl::list<Solutions::Registrars::Zero<Dim>>>
+class Zero : public ::AnalyticData<Dim, Registrars> {
+ public:
   using options = tmpl::list<>;
   static constexpr Options::String help{
       "The trivial solution, useful as initial guess."};
@@ -40,31 +51,46 @@ class Zero : public tt::ConformsTo<elliptic::protocols::AnalyticSolution> {
   Zero& operator=(const Zero&) noexcept = default;
   Zero(Zero&&) noexcept = default;
   Zero& operator=(Zero&&) noexcept = default;
-  ~Zero() noexcept = default;
+  ~Zero() noexcept override = default;
+
+  /// \cond
+  explicit Zero(CkMigrateMessage* /*unused*/) noexcept {}
+  using PUP::able::register_constructor;
+  WRAPPED_PUPable_decl_template(Zero);  // NOLINT
+  /// \endcond
 
   /// Retrieve a collection of variables at coordinates `x`
-  template <typename... Tags>
-  tuples::TaggedTuple<Tags...> variables(const tnsr::I<DataVector, Dim>& x,
-                                         tmpl::list<Tags...> /*meta*/) const
-      noexcept {
-    static_assert(tmpl::size<tmpl::list_difference<tmpl::list<Tags...>,
+  template <typename DataType, typename... RequestedTags>
+  tuples::TaggedTuple<RequestedTags...> variables(
+      const tnsr::I<DataType, Dim>& x,
+      tmpl::list<RequestedTags...> /*meta*/) const noexcept {
+    using supported_tags =
+        tmpl::list<Tags::Displacement<Dim>, Tags::Strain<Dim>,
+                   ::Tags::FixedSource<Tags::Displacement<Dim>>>;
+    static_assert(tmpl::size<tmpl::list_difference<tmpl::list<RequestedTags...>,
                                                    supported_tags>>::value == 0,
                   "The requested tag is not supported");
-    return {make_with_value<typename Tags::type>(x, 0.)...};
+    return {make_with_value<typename RequestedTags::type>(x, 0.)...};
   }
 
-  // clang-tidy: no pass by reference
-  void pup(PUP::er& /*p*/) noexcept {}  // NOLINT
+  // NOLINTNEXTLINE(google-runtime-references)
+  void pup(PUP::er& /*p*/) noexcept override {}
 };
 
-template <size_t Dim>
-bool operator==(const Zero<Dim>& /*lhs*/, const Zero<Dim>& /*rhs*/) noexcept {
+/// \cond
+template <size_t Dim, typename Registrars>
+PUP::able::PUP_ID Zero<Dim, Registrars>::my_PUP_ID = 0;  // NOLINT
+/// \endcond
+
+template <size_t Dim, typename Registrars>
+bool operator==(const Zero<Dim, Registrars>& /*lhs*/,
+                const Zero<Dim, Registrars>& /*rhs*/) noexcept {
   return true;
 }
 
-template <size_t Dim>
-bool operator!=(const Zero<Dim>& /*lhs*/, const Zero<Dim>& /*rhs*/) noexcept {
+template <size_t Dim, typename Registrars>
+bool operator!=(const Zero<Dim, Registrars>& /*lhs*/,
+                const Zero<Dim, Registrars>& /*rhs*/) noexcept {
   return false;
 }
-
 }  // namespace Elasticity::Solutions
