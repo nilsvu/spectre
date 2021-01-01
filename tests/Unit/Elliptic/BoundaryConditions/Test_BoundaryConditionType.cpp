7bf7b1385be04440e96591fcaf5c60953975d20e
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2021-01-01 20:16:37 +0100

    Add enum for elliptic boundary condition types
    This enum will be used to select the type of boundary conditions in
    some boundary condition classes that support such choice, e.g.
    when using an analytic solution or when imposing homogeneous (zero)
    boundary conditions. This choice can be made in the input file.

---
+++ tests/Unit/Elliptic/BoundaryConditions/Test_BoundaryConditionType.cpp
@@ -0,0 +1,23 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <string>
+
+#include "Elliptic/BoundaryConditions/BoundaryConditionType.hpp"
+#include "Framework/TestCreation.hpp"
+#include "Utilities/GetOutput.hpp"
+
+namespace elliptic {
+
+SPECTRE_TEST_CASE("Unit.Elliptic.BoundaryConditionType", "[Unit][Elliptic]") {
+  CHECK(get_output(BoundaryConditionType::Dirichlet) == "Dirichlet");
+  CHECK(get_output(BoundaryConditionType::Neumann) == "Neumann");
+  CHECK(TestHelpers::test_creation<BoundaryConditionType>("Dirichlet") ==
+        BoundaryConditionType::Dirichlet);
+  CHECK(TestHelpers::test_creation<BoundaryConditionType>("Neumann") ==
+        BoundaryConditionType::Neumann);
+}
+
+}  // namespace elliptic
