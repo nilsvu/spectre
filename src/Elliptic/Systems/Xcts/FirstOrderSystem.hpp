7e0bf0789670d3627eb26a09f5fe4c87ff695ae3
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2021-02-15 20:42:38 +0100

    WIP

--- src/Elliptic/Systems/Xcts/FirstOrderSystem.hpp
+++ src/Elliptic/Systems/Xcts/FirstOrderSystem.hpp
@@ -8,6 +8,8 @@
 #include "DataStructures/DataBox/Prefixes.hpp"
 #include "DataStructures/Tensor/EagerMath/Magnitude.hpp"
 #include "DataStructures/VariablesTag.hpp"
+#include "Elliptic/BoundaryConditions/AnalyticSolution.hpp"
+#include "Elliptic/BoundaryConditions/BoundaryCondition.hpp"
 #include "Elliptic/Systems/Xcts/Equations.hpp"
 #include "Elliptic/Systems/Xcts/Geometry.hpp"
 #include "Elliptic/Systems/Xcts/Tags.hpp"
@@ -223,6 +225,13 @@ struct FirstOrderSystem {
   using sources_computer = Sources<EnabledEquations, ConformalGeometry>;
   using sources_computer_linearized =
       LinearizedSources<EnabledEquations, ConformalGeometry>;
+
+  // The supported boundary conditions. Boundary conditions can be
+  // factory-created from this base class.
+  using boundary_conditions_base =
+      elliptic::BoundaryConditions::BoundaryCondition<
+          3, tmpl::list<elliptic::BoundaryConditions::Registrars::
+                            AnalyticSolution<FirstOrderSystem>>>;
 
   // The tag of the operator to compute magnitudes on the manifold, e.g. to
   // normalize vectors on the faces of an element
