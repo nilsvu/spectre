b1bde284295bb5277637b5acb0b4016b12edc75a
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2021-05-04 16:29:40 +0200

    Add Section struct and tag

---
+++ src/Parallel/Section.hpp
@@ -0,0 +1,90 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <charm++.h>
+#include <optional>
+#include <pup.h>
+
+#include "Parallel/ParallelComponentHelpers.hpp"
+
+namespace Parallel {
+
+/*!
+ * \brief A subset of chares in a parallel component
+ *
+ * The section is identified at compile time by the parallel component and a
+ * `SectionIdTag`. The `SectionIdTag` describes the quantity that partitions the
+ * chares into one or more sections. For example, the `SectionIdTag` could be
+ * the block ID in the computational domain, so elements are partitioned per
+ * block. Chares can be a member of multiple sections.
+ *
+ * Here's an example how to work with sections in an array parallel component:
+ *
+ * \snippet Test_SectionReductions.cpp sections_example
+ */
+template <typename ParallelComponent, typename SectionIdTag>
+struct Section {
+ private:
+  using chare_type = typename ParallelComponent::chare_type;
+  using charm_type = charm_types_with_parameters<
+      ParallelComponent,
+      typename get_array_index<chare_type>::template f<ParallelComponent>>;
+  using IdType = typename SectionIdTag::type;
+
+ public:
+  using parallel_component = ParallelComponent;
+  using cproxy_section = typename charm_type::cproxy_section;
+  using section_id_tag = SectionIdTag;
+
+  Section(IdType id, cproxy_section proxy) noexcept
+      : id_(id), proxy_(std::move(proxy)) {}
+
+  Section() = default;
+  Section(Section&& rhs) = default;
+  Section& operator=(Section&& rhs) = default;
+  Section(const Section&) = default;
+  Section& operator=(const Section&) = default;
+  ~Section() = default;
+
+  /// The section ID corresponding to the `SectionIdTag`
+  const IdType& id() const noexcept { return id_; }
+
+  // @{
+  /// The Charm++ section proxy
+  const cproxy_section& proxy() const noexcept { return proxy_; }
+  cproxy_section& proxy() noexcept { return proxy_; }
+  // @}
+
+  /*!
+   * \brief The Charm++ section cookie that keeps track of reductions
+   *
+   * The section cookie must be stored on each element and updated when
+   * performing reductions. For details on Charm++ sections and section
+   * reductions see:
+   * https://charm.readthedocs.io/en/latest/charm++/manual.html?#sections-subsets-of-a-chare-array-group
+   */
+  CkSectionInfo& cookie() const noexcept {
+    // Can't create the cookie in the constructor, possibly because the section
+    // elements need to exist at this point. Therefore we create it lazily here.
+    if (not cookie_.has_value()) {
+      cookie_ = proxy_.ckGetSectionInfo();
+    }
+    return *cookie_;
+  }
+
+  // NOLINTNEXTLINE(google-runtime-references)
+  void pup(PUP::er& p) noexcept {
+    p | id_;
+    p | proxy_;
+    p | cookie_;
+  }
+
+ private:
+  IdType id_{};
+  mutable cproxy_section proxy_{};
+  mutable std::optional<CkSectionInfo> cookie_{};
+};
+
+}  // namespace Parallel
