2ea83b1938f5cdef22e234e2a49bfead65e5341b
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2021-05-18 15:26:33 +0200

    WIP

--- src/PythonBindings/CharmCompatibility.cpp
+++ src/PythonBindings/CharmCompatibility.cpp
@@ -4,3 +4,9 @@
 // Charm looks for this function but since we build without a main function or
 // main module we just have it be empty
 extern "C" void CkRegisterMainModule(void) {}
+
+// Charm checks if this symbols exists in its build system and then references
+// it, but it is missing in Python binding libs because they are not compiled as
+// executables (at least that's what I think is happening). This upstream issue
+// is somewhat related: https://github.com/UIUC-PPL/charm/issues/1893
+// char __executable_start = '0';
