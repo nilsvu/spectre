7c55fa01e9504f225f4b5cf514e4d7762625cb51
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2021-08-25 00:59:28 +0200

    WIP kerr boost

--- src/PointwiseFunctions/AnalyticSolutions/GeneralRelativity/KerrSchild.cpp
+++ src/PointwiseFunctions/AnalyticSolutions/GeneralRelativity/KerrSchild.cpp
@@ -12,6 +12,7 @@
 #include "DataStructures/DataVector.hpp"  // IWYU pragma: keep
 #include "PointwiseFunctions/GeneralRelativity/ExtrinsicCurvature.hpp"
 #include "PointwiseFunctions/GeneralRelativity/Tags.hpp"
+#include "PointwiseFunctions/SpecialRelativity/LorentzBoostMatrix.hpp"
 #include "Utilities/ConstantExpressions.hpp"
 #include "Utilities/ContainerHelpers.hpp"
 #include "Utilities/GenerateInstantiations.hpp"
@@ -23,15 +24,14 @@
 namespace gr::Solutions {
 
 KerrSchild::KerrSchild(const double mass,
-                       KerrSchild::Spin::type dimensionless_spin,
-                       KerrSchild::Center::type center,
+                       const std::array<double, volume_dim> dimensionless_spin,
+                       const std::array<double, volume_dim> center,
+                       const std::array<double, volume_dim> boost_velocity,
                        const Options::Context& context)
     : mass_(mass),
-      // clang-tidy: do not std::move trivial types.
-      dimensionless_spin_(std::move(dimensionless_spin)),  // NOLINT
-      // clang-tidy: do not std::move trivial types.
-      center_(std::move(center))  // NOLINT
-{
+      dimensionless_spin_(dimensionless_spin),
+      center_(center),
+      boost_velocity_(boost_velocity) {
   const double spin_magnitude = magnitude(dimensionless_spin_);
   if (spin_magnitude > 1.0) {
     PARSE_ERROR(context, "Spin magnitude must be < 1. Given spin: "
@@ -47,6 +47,7 @@ void KerrSchild::pup(PUP::er& p) {
   p | mass_;
   p | dimensionless_spin_;
   p | center_;
+  p | boost_velocity_;
 }
 
 template <typename DataType, typename Frame>
@@ -59,11 +60,24 @@ template <typename DataType, typename Frame>
 void KerrSchild::IntermediateComputer<DataType, Frame>::operator()(
     const gsl::not_null<tnsr::I<DataType, 3, Frame>*> x_minus_center,
     const gsl::not_null<CachedBuffer*> /*cache*/,
-    internal_tags::x_minus_center<DataType, Frame> /*meta*/) const {
+    internal_tags::x_minus_center_unboosted<DataType, Frame> /*meta*/)
+    const {
   *x_minus_center = x_;
   for (size_t d = 0; d < 3; ++d) {
     x_minus_center->get(d) -= gsl::at(solution_.center(), d);
   }
+}
+
+template <typename DataType, typename Frame>
+void KerrSchild::IntermediateComputer<DataType, Frame>::operator()(
+    const gsl::not_null<tnsr::I<DataType, 3, Frame>*> x_minus_center_boosted,
+    const gsl::not_null<CachedBuffer*> cache,
+    internal_tags::x_minus_center<DataType, Frame> /*meta*/) const {
+  const auto& x_minus_center = cache->get_var(
+      internal_tags::x_minus_center_unboosted<DataType, Frame>{});
+  // Inverse-boost because the coordinates are given in the boosted frame
+  sr::lorentz_boost(x_minus_center_boosted, x_minus_center, 0.,
+                    -solution_.boost_velocity());
 }
 
 template <typename DataType, typename Frame>
@@ -230,9 +244,9 @@ void KerrSchild::IntermediateComputer<DataType, Frame>::operator()(
 
 template <typename DataType, typename Frame>
 void KerrSchild::IntermediateComputer<DataType, Frame>::operator()(
-    const gsl::not_null<tnsr::i<DataType, 3, Frame>*> deriv_H,
+    const gsl::not_null<tnsr::a<DataType, 3, Frame>*> deriv_H,
     const gsl::not_null<CachedBuffer*> cache,
-    internal_tags::deriv_H<DataType, Frame> /*meta*/) const {
+    internal_tags::deriv_H_unboosted<DataType, Frame> /*meta*/) const {
   const auto& deriv_log_r =
       cache->get_var(internal_tags::deriv_log_r<DataType, Frame>{});
   const auto& deriv_H_temp1 =
@@ -241,10 +255,21 @@ void KerrSchild::IntermediateComputer<DataType, Frame>::operator()(
       get(cache->get_var(internal_tags::deriv_H_temp2<DataType>{}));
 
   const auto spin_a = solution_.dimensionless_spin() * solution_.mass();
+  get<0>(*deriv_H) = 0.;
   for (size_t i = 0; i < 3; ++i) {
-    deriv_H->get(i) =
+    deriv_H->get(i + 1) =
         deriv_H_temp1 * deriv_log_r.get(i) - deriv_H_temp2 * gsl::at(spin_a, i);
   }
+}
+
+template <typename DataType, typename Frame>
+void KerrSchild::IntermediateComputer<DataType, Frame>::operator()(
+    const gsl::not_null<tnsr::a<DataType, 3, Frame>*> deriv_H_boosted,
+    const gsl::not_null<CachedBuffer*> cache,
+    internal_tags::deriv_H<DataType, Frame> /*meta*/) const {
+  const auto& deriv_H =
+      cache->get_var(internal_tags::deriv_H_unboosted<DataType, Frame>{});
+  sr::lorentz_boost(deriv_H_boosted, deriv_H, -solution_.boost_velocity());
 }
 
 template <typename DataType, typename Frame>
@@ -275,9 +300,9 @@ void KerrSchild::IntermediateComputer<DataType, Frame>::operator()(
 
 template <typename DataType, typename Frame>
 void KerrSchild::IntermediateComputer<DataType, Frame>::operator()(
-    const gsl::not_null<tnsr::i<DataType, 3, Frame>*> null_form,
+    const gsl::not_null<tnsr::a<DataType, 3, Frame>*> null_form,
     const gsl::not_null<CachedBuffer*> cache,
-    internal_tags::null_form<DataType, Frame> /*meta*/) const {
+    internal_tags::null_form_unboosted<DataType, Frame> /*meta*/) const {
   const auto& a_dot_x_over_r =
       get(cache->get_var(internal_tags::a_dot_x_over_r<DataType>{}));
   const auto& r = get(cache->get_var(internal_tags::r<DataType>{}));
@@ -286,52 +311,69 @@ void KerrSchild::IntermediateComputer<DataType, Frame>::operator()(
   const auto& denom = get(cache->get_var(internal_tags::denom<DataType>{}));
 
   const auto spin_a = solution_.dimensionless_spin() * solution_.mass();
+  get<0>(*null_form) = 1.;
   for (size_t i = 0; i < 3; ++i) {
     const size_t cross_product_index_1 = (i + 1) % 3;
     const size_t cross_product_index_2 = (i + 2) % 3;
-    null_form->get(i) = denom * (r * x_minus_center.get(i) -
-                                 gsl::at(spin_a, cross_product_index_1) *
-                                     x_minus_center.get(cross_product_index_2) +
-                                 gsl::at(spin_a, cross_product_index_2) *
-                                     x_minus_center.get(cross_product_index_1) +
-                                 a_dot_x_over_r * gsl::at(spin_a, i));
+    null_form->get(i + 1) =
+        denom * (r * x_minus_center.get(i) -
+                 gsl::at(spin_a, cross_product_index_1) *
+                     x_minus_center.get(cross_product_index_2) +
+                 gsl::at(spin_a, cross_product_index_2) *
+                     x_minus_center.get(cross_product_index_1) +
+                 a_dot_x_over_r * gsl::at(spin_a, i));
   }
 }
 
 template <typename DataType, typename Frame>
 void KerrSchild::IntermediateComputer<DataType, Frame>::operator()(
-    const gsl::not_null<tnsr::ij<DataType, 3, Frame>*> deriv_null_form,
+    const gsl::not_null<tnsr::a<DataType, 3, Frame>*> null_form_boosted,
     const gsl::not_null<CachedBuffer*> cache,
-    internal_tags::deriv_null_form<DataType, Frame> /*meta*/) const {
+    internal_tags::null_form<DataType, Frame> /*meta*/) const {
+  const auto& null_form =
+      cache->get_var(internal_tags::null_form_unboosted<DataType, Frame>{});
+  sr::lorentz_boost(null_form_boosted, null_form, -solution_.boost_velocity());
+}
+
+template <typename DataType, typename Frame>
+void KerrSchild::IntermediateComputer<DataType, Frame>::operator()(
+    const gsl::not_null<tnsr::ab<DataType, 3, Frame>*> deriv_null_form,
+    const gsl::not_null<CachedBuffer*> cache,
+    internal_tags::deriv_null_form_unboosted<DataType, Frame> /*meta*/)
+    const {
   const auto spin_a = solution_.dimensionless_spin() * solution_.mass();
   const auto& denom = get(cache->get_var(internal_tags::denom<DataType>{}));
   const auto& r = get(cache->get_var(internal_tags::r<DataType>{}));
   const auto& x_minus_center =
       cache->get_var(internal_tags::x_minus_center<DataType, Frame>{});
   const auto& null_form =
-      cache->get_var(internal_tags::null_form<DataType, Frame>{});
+      cache->get_var(internal_tags::null_form_unboosted<DataType, Frame>{});
   const auto& a_dot_x_over_rsquared =
       get(cache->get_var(internal_tags::a_dot_x_over_rsquared<DataType>{}));
   const auto& deriv_log_r =
       cache->get_var(internal_tags::deriv_log_r<DataType, Frame>{});
 
+  for (size_t d = 0; d < 4; ++d) {
+    deriv_null_form->get(d, 0) = 0.;
+    deriv_null_form->get(0, d) = 0.;
+  }
   for (size_t i = 0; i < 3; i++) {
     for (size_t j = 0; j < 3; j++) {
-      deriv_null_form->get(j, i) =
+      deriv_null_form->get(j + 1, i + 1) =
           denom * (gsl::at(spin_a, i) * gsl::at(spin_a, j) / r +
-                   (x_minus_center.get(i) - 2.0 * r * null_form.get(i) -
+                   (x_minus_center.get(i) - 2.0 * r * null_form.get(i + 1) -
                     a_dot_x_over_rsquared * gsl::at(spin_a, i)) *
                        deriv_log_r.get(j) * r);
       if (i == j) {
-        deriv_null_form->get(j, i) += denom * r;
+        deriv_null_form->get(j + 1, i + 1) += denom * r;
       } else {  //  add denom*epsilon^ijk a_k
         size_t k = (j + 1) % 3;
         if (k == i) {  // j+1 = i (cyclic), so choose minus sign
           k++;
           k = k % 3;  // and set k to be neither i nor j
-          deriv_null_form->get(j, i) -= denom * gsl::at(spin_a, k);
+          deriv_null_form->get(j + 1, i + 1) -= denom * gsl::at(spin_a, k);
         } else {  // i+1 = j (cyclic), so choose plus sign
-          deriv_null_form->get(j, i) += denom * gsl::at(spin_a, k);
+          deriv_null_form->get(j + 1, i + 1) += denom * gsl::at(spin_a, k);
         }
       }
     }
@@ -340,11 +382,26 @@ void KerrSchild::IntermediateComputer<DataType, Frame>::operator()(
 
 template <typename DataType, typename Frame>
 void KerrSchild::IntermediateComputer<DataType, Frame>::operator()(
+    const gsl::not_null<tnsr::ab<DataType, 3, Frame>*> deriv_null_form_boosted,
+    const gsl::not_null<CachedBuffer*> cache,
+    internal_tags::deriv_null_form<DataType, Frame> /*meta*/) const {
+  const auto& deriv_null_form = cache->get_var(
+      internal_tags::deriv_null_form_unboosted<DataType, Frame>{});
+  // Inverse-boost the first index, because it represents the derivative which
+  // is given in the boosted frame
+  sr::lorentz_boost(deriv_null_form_boosted, deriv_null_form,
+                    -solution_.boost_velocity(), -solution_.boost_velocity());
+}
+
+template <typename DataType, typename Frame>
+void KerrSchild::IntermediateComputer<DataType, Frame>::operator()(
     const gsl::not_null<Scalar<DataType>*> lapse_squared,
     const gsl::not_null<CachedBuffer*> cache,
     internal_tags::lapse_squared<DataType> /*meta*/) const {
   const auto& H = get(cache->get_var(internal_tags::H<DataType>{}));
-  get(*lapse_squared) = 1.0 / (1.0 + 2.0 * square(null_vector_0_) * H);
+  const auto& null_form =
+      cache->get_var(internal_tags::null_form<DataType, Frame>{});
+  get(*lapse_squared) = 1.0 / (1.0 + 2.0 * square(get<0>(null_form)) * H);
 }
 
 template <typename DataType, typename Frame>
@@ -365,8 +422,7 @@ void KerrSchild::IntermediateComputer<DataType, Frame>::operator()(
   const auto& lapse = get(cache->get_var(gr::Tags::Lapse<DataType>{}));
   const auto& lapse_squared =
       get(cache->get_var(internal_tags::lapse_squared<DataType>{}));
-  get(*deriv_lapse_multiplier) =
-      -square(null_vector_0_) * lapse * lapse_squared;
+  get(*deriv_lapse_multiplier) = -lapse * lapse_squared;
 }
 
 template <typename DataType, typename Frame>
@@ -375,10 +431,12 @@ void KerrSchild::IntermediateComputer<DataType, Frame>::operator()(
     const gsl::not_null<CachedBuffer*> cache,
     internal_tags::shift_multiplier<DataType> /*meta*/) const {
   const auto& H = get(cache->get_var(internal_tags::H<DataType>{}));
+  const auto& null_form =
+      cache->get_var(internal_tags::null_form<DataType, Frame>{});
   const auto& lapse_squared =
       get(cache->get_var(internal_tags::lapse_squared<DataType>{}));
 
-  get(*shift_multiplier) = -2.0 * null_vector_0_ * H * lapse_squared;
+  get(*shift_multiplier) = 2.0 * get<0>(null_form) * H * lapse_squared;
 }
 
 template <typename DataType, typename Frame>
@@ -392,7 +450,7 @@ void KerrSchild::IntermediateComputer<DataType, Frame>::operator()(
       get(cache->get_var(internal_tags::shift_multiplier<DataType>{}));
 
   for (size_t i = 0; i < 3; ++i) {
-    shift->get(i) = shift_multiplier * null_form.get(i);
+    shift->get(i) = shift_multiplier * null_form.get(i + 1);
   }
 }
 
@@ -413,12 +471,15 @@ void KerrSchild::IntermediateComputer<DataType, Frame>::operator()(
 
   for (size_t m = 0; m < 3; ++m) {
     for (size_t i = 0; i < 3; ++i) {
-      deriv_shift->get(m, i) = 4.0 * cube(null_vector_0_) * H *
-                                   null_form.get(i) * square(lapse_squared) *
-                                   deriv_H.get(m) -
-                               2.0 * null_vector_0_ * lapse_squared *
-                                   (null_form.get(i) * deriv_H.get(m) +
-                                    H * deriv_null_form.get(m, i));
+      deriv_shift->get(m, i) =
+          2.0 * lapse_squared *
+              (get<0>(null_form) * null_form.get(i + 1) * deriv_H.get(m + 1) +
+               H * get<0>(null_form) * deriv_null_form.get(m + 1, i + 1) +
+               H * null_form.get(i + 1) * deriv_null_form.get(m + 1, 0)) -
+          4.0 * H * get<0>(null_form) * null_form.get(i + 1) *
+              square(lapse_squared) *
+              (square(get<0>(null_form)) * deriv_H.get(m + 1) +
+               2. * H * get<0>(null_form) * deriv_null_form.get(m + 1, 0));
     }
   }
 }
@@ -437,7 +498,7 @@ void KerrSchild::IntermediateComputer<DataType, Frame>::operator()(
     spatial_metric->get(i, i) = 1.;
     for (size_t j = i; j < 3; ++j) {  // Symmetry
       spatial_metric->get(i, j) +=
-          2.0 * H * null_form.get(i) * null_form.get(j);
+          2.0 * H * null_form.get(i + 1) * null_form.get(j + 1);
     }
   }
 }
@@ -459,10 +520,11 @@ void KerrSchild::IntermediateComputer<DataType, Frame>::operator()(
     for (size_t j = i; j < 3; ++j) {  // Symmetry
       for (size_t m = 0; m < 3; ++m) {
         deriv_spatial_metric->get(m, i, j) =
-            2.0 * null_form.get(i) * null_form.get(j) * deriv_H.get(m) +
+            2.0 * null_form.get(i + 1) * null_form.get(j + 1) *
+                deriv_H.get(m + 1) +
             2.0 * H *
-                (null_form.get(i) * deriv_null_form.get(m, j) +
-                 null_form.get(j) * deriv_null_form.get(m, i));
+                (null_form.get(i + 1) * deriv_null_form.get(m + 1, j + 1) +
+                 null_form.get(j + 1) * deriv_null_form.get(m + 1, i + 1));
       }
     }
   }
@@ -487,12 +549,19 @@ tnsr::i<DataType, 3, Frame>
 KerrSchild::IntermediateVars<DataType, Frame>::get_var(
     DerivLapse<DataType, Frame> /*meta*/) {
   tnsr::i<DataType, 3, Frame> result{};
+  const auto& H = get_var(internal_tags::H<DataType>{});
   const auto& deriv_H = get_var(internal_tags::deriv_H<DataType, Frame>{});
   const auto& deriv_lapse_multiplier =
       get(get_var(internal_tags::deriv_lapse_multiplier<DataType>{}));
+  const auto& null_form = get_var(internal_tags::null_form<DataType, Frame>{});
+  const auto& deriv_null_form =
+      get_var(internal_tags::deriv_null_form<DataType, Frame>{});
 
   for (size_t i = 0; i < 3; ++i) {
-    result.get(i) = deriv_lapse_multiplier * deriv_H.get(i);
+    result.get(i) =
+        deriv_lapse_multiplier *
+        (square(get<0>(null_form)) * deriv_H.get(i + 1) +
+         2. * get(H) * get<0>(null_form) * deriv_null_form.get(i + 1, 0));
   }
   return result;
 }
@@ -532,7 +601,7 @@ KerrSchild::IntermediateVars<DataType, Frame>::get_var(
     result.get(i, i) = 1.;
     for (size_t j = i; j < 3; ++j) {  // Symmetry
       result.get(i, j) -=
-          2.0 * H * lapse_squared * null_form.get(i) * null_form.get(j);
+          2.0 * H * lapse_squared * null_form.get(i + 1) * null_form.get(j + 1);
     }
   }
 
