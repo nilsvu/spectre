1963799dff6d573168fabcbc19818e0df68b194c
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2021-09-14 10:46:09 +0200

    Add DataVector overload to IrregularInterpolant

--- src/NumericalAlgorithms/Interpolation/IrregularInterpolant.hpp
+++ src/NumericalAlgorithms/Interpolation/IrregularInterpolant.hpp
@@ -56,6 +56,18 @@ class Irregular {
   Variables<TagsList> interpolate(const Variables<TagsList>& vars) const;
   /// @}
 
+  /// @{
+  /// \brief Interpolate a DataVector onto the target points.
+  ///
+  /// \note When interpolating multiple tensors, the Variables interface is more
+  /// efficient. However, this DataVector interface is useful for applications
+  /// where only some components of a Tensor or Variables need to be
+  /// interpolated.
+  void interpolate(gsl::not_null<DataVector*> result,
+                   const DataVector& input) const;
+  DataVector interpolate(const DataVector& input) const;
+  /// @}
+
  private:
   friend bool operator==(const Irregular& lhs, const Irregular& rhs) {
     return lhs.interpolation_matrix_ == rhs.interpolation_matrix_;
@@ -89,6 +101,24 @@ void Irregular<Dim>::interpolate(
 }
 
 template <size_t Dim>
+void Irregular<Dim>::interpolate(const gsl::not_null<DataVector*> result,
+                                 const DataVector& input) const {
+  const size_t m = interpolation_matrix_.rows();
+  const size_t k = interpolation_matrix_.columns();
+  ASSERT(k == input.size(),
+         "Number of points in 'input', "
+             << input.size()
+             << ",\n disagrees with the size of the source_mesh, " << k
+             << ", that was passed into the constructor");
+  if (result->size() != m) {
+    *result = DataVector{m};
+  }
+  dgemm_('n', 'n', m, 1, k, 1.0, interpolation_matrix_.data(),
+         interpolation_matrix_.spacing(), input.data(), k, 0.0, result->data(),
+         m);
+}
+
+template <size_t Dim>
 template <typename TagsList>
 Variables<TagsList> Irregular<Dim>::interpolate(
     const Variables<TagsList>& vars) const {
@@ -98,7 +128,13 @@ Variables<TagsList> Irregular<Dim>::interpolate(
 }
 
 template <size_t Dim>
+DataVector Irregular<Dim>::interpolate(const DataVector& input) const {
+  DataVector result{input.size()};
+  interpolate(make_not_null(&result), input);
+  return result;
+}
+
+template <size_t Dim>
 bool operator!=(const Irregular<Dim>& lhs, const Irregular<Dim>& rhs);
 
 }  // namespace intrp
-
