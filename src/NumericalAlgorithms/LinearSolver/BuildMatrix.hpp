fb12331b8b295a35813000f6e0acd2f58d43ca45
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2021-09-16 00:14:10 +0200

    WIP ILU

--- src/NumericalAlgorithms/LinearSolver/BuildMatrix.hpp
+++ src/NumericalAlgorithms/LinearSolver/BuildMatrix.hpp
@@ -3,11 +3,15 @@
 
 #pragma once
 
+#include <Eigen/SparseCore>
 #include <algorithm>
 #include <cstddef>
 #include <tuple>
+#include <vector>
 
 #include "DataStructures/DenseMatrix.hpp"
+#include "Utilities/ConstantExpressions.hpp"
+#include "Utilities/EqualWithinRoundoff.hpp"
 #include "Utilities/Gsl.hpp"
 #include "Utilities/TypeTraits/CreateIsCallable.hpp"
 
@@ -27,16 +31,17 @@ CREATE_IS_CALLABLE_V(reset)
  */
 template <typename LinearOperator, typename OperandType, typename ResultType,
           typename... OperatorArgs>
-void build_matrix(
-    const gsl::not_null<DenseMatrix<double, blaze::columnMajor>*> matrix,
-    const gsl::not_null<OperandType*> operand_buffer,
-    const gsl::not_null<ResultType*> result_buffer,
-    const LinearOperator& linear_operator,
-    const std::tuple<OperatorArgs...>& operator_args) {
+void build_matrix(const gsl::not_null<Eigen::SparseMatrix<double>*> matrix,
+                  const gsl::not_null<OperandType*> operand_buffer,
+                  const gsl::not_null<ResultType*> result_buffer,
+                  const LinearOperator& linear_operator,
+                  const std::tuple<OperatorArgs...>& operator_args) {
   size_t i = 0;
   // Re-using the iterators for all operator invocations
   auto result_iterator_begin = result_buffer->begin();
   auto result_iterator_end = result_buffer->end();
+  std::vector<Eigen::Triplet<double>> sparse_entries{};
+  sparse_entries.reserve(square(static_cast<size_t>(matrix->rows())));
   for (double& unit_vector_data : *operand_buffer) {
     // Add a 1 at the unit vector location i
     unit_vector_data = 1.;
@@ -57,10 +62,15 @@ void build_matrix(
       result_iterator_end = result_buffer->end();
     }
     // Store the result in column i of the matrix
-    std::copy(result_iterator_begin, result_iterator_end,
-              column(*matrix, i).begin());
+    for (size_t j = 0; j < static_cast<size_t>(matrix->rows()); ++j) {
+      if (not equal_within_roundoff(*result_iterator_begin, 0.)) {
+        sparse_entries.emplace_back(j, i, *result_iterator_begin);
+      }
+      ++result_iterator_begin;
+    }
     ++i;
   }
+  matrix->setFromTriplets(sparse_entries.begin(), sparse_entries.end());
 }
 
 }  // namespace LinearSolver::Serial
