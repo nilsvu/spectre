7defec6cd11ab86fa27eb2c89652b8009fd87f7b
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2021-09-18 16:36:27 +0200

    WIP oversample constraints

--- src/PointwiseFunctions/Xcts/SpacetimeQuantities.hpp
+++ src/PointwiseFunctions/Xcts/SpacetimeQuantities.hpp
@@ -4,6 +4,7 @@
 #pragma once
 
 #include <cstddef>
+#include <optional>
 
 #include "DataStructures/CachedTempBuffer.hpp"
 #include "DataStructures/DataBox/Prefixes.hpp"
@@ -11,10 +12,14 @@
 #include "DataStructures/DataVector.hpp"
 #include "DataStructures/Tensor/Tensor.hpp"
 #include "DataStructures/VariablesTag.hpp"
+#include "Domain/ElementMap.hpp"
+#include "Domain/LogicalCoordinates.hpp"
 #include "Domain/Tags.hpp"
 #include "Elliptic/Systems/Xcts/Tags.hpp"
+#include "NumericalAlgorithms/Interpolation/RegularGridInterpolant.hpp"
 #include "NumericalAlgorithms/LinearOperators/PartialDerivatives.hpp"
 #include "NumericalAlgorithms/Spectral/Mesh.hpp"
+#include "Options/Auto.hpp"
 #include "PointwiseFunctions/GeneralRelativity/Tags.hpp"
 #include "Utilities/Gsl.hpp"
 #include "Utilities/TMPL.hpp"
@@ -140,28 +145,81 @@ struct SpacetimeQuantitiesComputer {
       inv_jacobian;
 };
 
+namespace OptionTags {
+struct Constraints {
+  static constexpr Options::String help = "Options for computing constraints";
+};
+template <size_t Dim>
+struct OversampleMesh : db::SimpleTag {
+  using type = Options::Auto<::Mesh<Dim>, Options::AutoLabel::None>;
+  using group = Constraints;
+  static constexpr Options::String help = "Compute constraints on this mesh.";
+};
+}  // namespace OptionTags
+
 namespace Tags {
+
+template <size_t Dim>
+struct ConstraintsOversampleMesh : db::SimpleTag {
+  using type = std::optional<::Mesh<Dim>>;
+  using option_tags = tmpl::list<OptionTags::OversampleMesh<Dim>>;
+  static constexpr bool pass_metavariables = false;
+  static type create_from_options(const type& value) noexcept { return value; }
+};
+
 /// Compute tag for the 3+1 quantities `Tags` from XCTS variables. The `Tags`
 /// can be any subset of the tags supported by `Xcts::SpacetimeQuantities`.
-template <typename Tags>
+template <typename BackgroundTag, typename Tags>
 struct SpacetimeQuantitiesCompute : ::Tags::Variables<Tags>, db::ComputeTag {
+ private:
+  using vars_tags = tmpl::list<ConformalFactor<DataVector>,
+                               LapseTimesConformalFactor<DataVector>,
+                               ShiftExcess<DataVector, 3, Frame::Inertial>>;
+  using background_tags =
+      tmpl::list<ConformalMetric<DataVector, 3, Frame::Inertial>,
+                 InverseConformalMetric<DataVector, 3, Frame::Inertial>,
+                 ShiftBackground<DataVector, 3, Frame::Inertial>>;
+
+ public:
   using base = ::Tags::Variables<Tags>;
-  using argument_tags = tmpl::list<
-      domain::Tags::Mesh<3>, ConformalFactor<DataVector>,
-      LapseTimesConformalFactor<DataVector>,
-      ShiftExcess<DataVector, 3, Frame::Inertial>,
-      ConformalMetric<DataVector, 3, Frame::Inertial>,
-      InverseConformalMetric<DataVector, 3, Frame::Inertial>,
-      ShiftBackground<DataVector, 3, Frame::Inertial>, domain::Tags::Mesh<3>,
-      domain::Tags::InverseJacobian<3, Frame::Logical, Frame::Inertial>>;
-  template <typename... Args>
+  using argument_tags =
+      tmpl::list<::Tags::Variables<vars_tags>, domain::Tags::Mesh<3>,
+                 domain::Tags::ElementMap<3>, ConstraintsOversampleMesh<3>,
+                 BackgroundTag>;
+  template <typename Background>
   static void function(const gsl::not_null<typename base::type*> result,
-                       const Mesh<3>& mesh, const Args&... args) noexcept {
+                       const ::Variables<vars_tags>& original_vars,
+                       const Mesh<3>& original_mesh,
+                       const ElementMap<3, Frame::Inertial>& element_map,
+                       const std::optional<Mesh<3>>& oversample_mesh,
+                       const Background& background) noexcept {
+    const Mesh<3>& mesh = oversample_mesh.value_or(original_mesh);
     const size_t num_points = mesh.number_of_grid_points();
     if (result->number_of_grid_points() != num_points) {
       result->initialize(num_points);
     }
-    SpacetimeQuantities spacetime_quantities{num_points, {args...}};
+    // Interpolate solved variables to oversampled mesh
+    const intrp::RegularGrid<3> interpolant{original_mesh, mesh};
+    const auto vars = interpolant.interpolate(original_vars);
+    // Evaluate background quantities on oversampled mesh
+    const auto logical_coords = logical_coordinates(mesh);
+    const auto inertial_coords = element_map(logical_coords);
+    const auto inv_jacobian = element_map.inv_jacobian(logical_coords);
+    const auto background_quantities =
+        background.variables(inertial_coords, background_tags{});
+    // Compute spacetime quantities
+    SpacetimeQuantities spacetime_quantities{
+        num_points,
+        {get<ConformalFactor<DataVector>>(vars),
+         get<LapseTimesConformalFactor<DataVector>>(vars),
+         get<ShiftExcess<DataVector, 3, Frame::Inertial>>(vars),
+         get<ConformalMetric<DataVector, 3, Frame::Inertial>>(
+             background_quantities),
+         get<InverseConformalMetric<DataVector, 3, Frame::Inertial>>(
+             background_quantities),
+         get<ShiftBackground<DataVector, 3, Frame::Inertial>>(
+             background_quantities),
+         mesh, inv_jacobian}};
     tmpl::for_each<Tags>(
         [&spacetime_quantities, &result](const auto tag_v) noexcept {
           using tag = tmpl::type_from<std::decay_t<decltype(tag_v)>>;
