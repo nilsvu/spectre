db28d4928bb88d0d1f15d85d83acee157e68e15f
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2021-10-02 17:39:10 +0200

    Increase max num points

--- src/NumericalAlgorithms/Spectral/Spectral.hpp
+++ src/NumericalAlgorithms/Spectral/Spectral.hpp
@@ -141,7 +141,7 @@ constexpr size_t minimum_number_of_points =
  */
 template <Basis basis>
 constexpr size_t maximum_number_of_points =
-    basis == Basis::FiniteDifference ? 24 : 12;
+    basis == Basis::FiniteDifference ? 24 : 16;
 
 /*!
  * \brief Compute the function values of the basis function \f$\Phi_k(x)\f$
