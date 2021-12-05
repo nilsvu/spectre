337d5d5fec675cfbd105974190e9b52f0e83a0d7
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2021-12-05 13:53:09 +0100

    Add XCTS TOV solution

--- src/PointwiseFunctions/AnalyticSolutions/Xcts/Factory.hpp
+++ src/PointwiseFunctions/AnalyticSolutions/Xcts/Factory.hpp
@@ -6,6 +6,7 @@
 #include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/KerrSchild.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/Xcts/Flatness.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/Xcts/Schwarzschild.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/Xcts/TovStar.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/Xcts/WrappedGr.hpp"
 #include "Utilities/TMPL.hpp"
 
@@ -13,6 +14,7 @@ namespace Xcts {
 /// Analytic solutions of the XCTS equations
 namespace Solutions {
 using all_analytic_solutions =
-    tmpl::list<Flatness, WrappedGr<gr::Solutions::KerrSchild>, Schwarzschild>;
+    tmpl::list<Flatness, WrappedGr<gr::Solutions::KerrSchild>, Schwarzschild,
+               TovStar>;
 }  // namespace Solutions
 }  // namespace Xcts
