6f8596c0d0ca40e04a18896eb5846449d6dbd52a
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2021-12-20 20:34:27 +0100

    Test clean_output and add to Python module

---
+++ tests/tools/Test_CleanOutput.py
@@ -0,0 +1,14 @@
+# Distributed under the MIT License.
+# See LICENSE.txt for details.
+
+import unittest
+from spectre.tools.CleanOutput import clean_output
+
+
+class TestCleanOutout(unittest.TestCase):
+    def test_clean_output(self):
+        self.assertTrue(False, "TODO")
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
