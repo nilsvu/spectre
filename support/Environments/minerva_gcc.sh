7d67b3beb3ec880644d98b0396aaaea9b8bcd5a0
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2021-12-29 03:56:53 +0100

    WIP spack env

--- support/Environments/minerva_gcc.sh
+++ support/Environments/minerva_gcc.sh
@@ -25,15 +25,16 @@
 #    ```sh
 #    export SPECTRE_HOME=path/to/spectre
 #    ```
+#    Note: Add this step to your `.bashrc` file if you don't want to repeat it
+#    every time you log in.
 # 4. Source this script and setup modules:
 #    ```sh
 #    source $SPECTRE_HOME/support/Environments/minerva_gcc.sh`
+#    module purge
 #    spectre_setup_modules
 #    ```
-#    Note: Add steps 3 and 4 to your `.bashrc` file if you don't want to repeat
-#    them every time you log in. The `spectre_setup_modules` function only
-#    adjusts your `MODULEPATH` to make the installed modules visible but doesn't
-#    load any, so it's safe to add to your `.bashrc`.
+#    This may take a while, because it builds all dependencies. You only have
+#    to do this once, or when you want to rebuild your dependency tree.
 # 5. Create a build directory, if you don't have one already. A good place is in
 #    your `/work` directory on Minerva:
 #    ```sh
@@ -47,10 +48,13 @@
 #    ```sh
 #    cd path/to/build/directory
 #    module purge
+#    spectre_load_modules
 #    spectre_run_cmake
 #    ```
 #    Note: Remember to `module purge` to work in a clean environment, unless you
-#    have reasons not to.
+#    have reasons not to. When you are coming directly from
+#    `spectre_setup_modules`, you can skip the `module purge` and
+#    `spectre_load_modules`.
 # 7. Compile! With the build directory configured, this script sourced and
 #    modules set up, you can skip the previous steps from now on.
 #    ```sh
@@ -73,28 +77,94 @@
 #    sbatch Minerva.sh
 #    ```
 
+spectre_load_sys_modules() {
+    module load gcc-10.2.0-gcc-10.2.0-vaerku7
+    module load binutils-2.36.1-gcc-10.2.0-wtzd7wm
+    module load git-2.29.0-gcc-10.2.0-m4kt4vn
+    module load python-3.8.7-gcc-10.2.0-pc7wahl
+}
+
+spectre_unload_sys_modules() {
+    module unload gcc-10.2.0-gcc-10.2.0-vaerku7
+    module unload binutils-2.36.1-gcc-10.2.0-wtzd7wm
+    module unload git-2.29.0-gcc-10.2.0-m4kt4vn
+    module unload python-3.8.7-gcc-10.2.0-pc7wahl
+}
+
+spectre_upgrade_environment() {
+    # Use this command to update the .lock file in the repo
+
+    spectre_load_sys_modules
+
+    # Recreate the environment from scratch
+    spack env deactivate
+    spack env remove -y spectre_minerva_gcc
+    spack env create spectre_minerva_gcc \
+      $SPECTRE_HOME/support/DevEnvironments/spack.yaml
+    spack env activate spectre_minerva_gcc -p
+    spack compiler find
+    spack external find
+
+    # Configure Charm++ installation
+    spack remove charmpp && spack add \
+      intel-mpi charmpp@6.10.2 backend=mpi scotch
+
+    # Concretize
+    spack concretize -f --reuse
+    cp $SPECTRE_HOME/support/spack/var/spack/environments/spectre_minerva_gcc/spack.lock \
+      $SPECTRE_HOME/support/Environments/minerva_gcc.lock
+}
+
 spectre_setup_modules() {
-    export MODULEPATH="\
-/home/SPACK2021/share/spack/modules/linux-centos7-haswell:$MODULEPATH"
-    export MODULEPATH="\
-/home/nfischer/spack/share/spack/modules/linux-centos7-haswell:$MODULEPATH"
+    if [ -z ${SPECTRE_HOME} ]; then
+        echo "You must set SPECTRE_HOME to the cloned SpECTRE directory"
+        return 1
+    fi
+
+    spectre_load_sys_modules
+
+    # Install or upgrade Spack in $SPECTRE_HOME/support/spack
+    if [ -f $SPECTRE_HOME/support/spack/share/spack/setup-env.sh ]; then
+        git -C $SPECTRE_HOME/support/spack/ pull
+    else
+        git clone -c feature.manyFiles=true \
+          https://github.com/spack/spack.git $SPECTRE_HOME/support/spack
+        git -C $SPECTRE_HOME/support/spack/ apply \
+          $SPECTRE_HOME/support/Environments/spack.patch
+        # Add upstream install tree to reuse existing packages
+        cat >$SPECTRE_HOME/support/spack/etc/spack/upstreams.yaml <<EOF
+upstreams:
+  spack2021:
+    install_tree: /home/SPACK2021/opt/spack
+    modules:
+      lmod: /home/SPACK2021/share/spack/modules
+EOF
+    fi
+
+    # Create environment from the lockfile
+    source $SPECTRE_HOME/support/spack/share/spack/setup-env.sh
+    spack env deactivate
+    spack env remove -y spectre_minerva_gcc
+    spack env create spectre_minerva_gcc \
+      $SPECTRE_HOME/support/Environments/minerva_gcc.lock
+    spack env activate spectre_minerva_gcc -p
+
+    # Install
+    spack install --only-concrete
+    spack env loads -r
+    spack find -x
 }
 
 spectre_load_modules() {
-    module load gcc-10.2.0-gcc-10.2.0-vaerku7
-    module load binutils-2.36.1-gcc-10.2.0-wtzd7wm
-    source /home/nfischer/spack/var/spack/environments/spectre_2021-03-18/loads
-    export CHARM_ROOT="\
-/home/nfischer/spack/opt/spack/linux-centos7-haswell/gcc-10.2.0/\
-charmpp-6.10.2-2waqdh24tz4yt5ozy5clhx6ahxjgivwz"
-    # Libsharp is installed separately with `-fPIC` since the Spack package
-    # doesn't support that option (yet)
-    export LIBSHARP_ROOT="/work/nfischer/spectre/libsharp_2021-03-18/auto"
+    spectre_load_sys_modules
+    source $SPECTRE_HOME/support/spack/share/spack/setup-env.sh
+    spack env activate spectre_minerva_gcc -p
+    export CHARM_ROOT=`spack location --install-dir charmpp`
 }
 
 spectre_unload_modules() {
-    echo "Unloading a subset of modules is not supported."
-    echo "Run 'module purge' to unload all modules."
+    spack env deactivate
+    spectre_unload_sys_modules
 }
 
 spectre_run_cmake() {
@@ -102,13 +172,11 @@ spectre_run_cmake() {
         echo "You must set SPECTRE_HOME to the cloned SpECTRE directory"
         return 1
     fi
-    spectre_load_modules
     cmake \
       -D CMAKE_C_COMPILER=gcc \
       -D CMAKE_CXX_COMPILER=g++ \
       -D CMAKE_Fortran_COMPILER=gfortran \
       -D CHARM_ROOT=$CHARM_ROOT \
-      -D LIBSHARP_ROOT=$LIBSHARP_ROOT \
       -D CMAKE_BUILD_TYPE=Release \
       -D DEBUG_SYMBOLS=OFF \
       -D BUILD_SHARED_LIBS=ON \
