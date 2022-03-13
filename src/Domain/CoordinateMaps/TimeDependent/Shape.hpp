33ba75b9e09e8dba9dc3d8ca5027e19c2968b2e3
Nils Vu <owls@nilsvu.de>
2022-03-13 15:24:39 +0100

    Shape map: support static map, fix and extend docs, add test
    This commit allows to use the shape map without a FunctionOfTime,
    e.g., to excise a Kerr-horizon shape in the BBH initial data domain.
    
    Also clarify docs and add an extensive test.

--- src/Domain/CoordinateMaps/TimeDependent/Shape.hpp
+++ src/Domain/CoordinateMaps/TimeDependent/Shape.hpp
@@ -3,11 +3,14 @@
 
 #pragma once
 
+#include <array>
 #include <cstddef>
 #include <memory>
 #include <optional>
 #include <string>
+#include <unordered_map>
 
+#include "DataStructures/DataVector.hpp"
 #include "DataStructures/Tensor/Tensor.hpp"
 #include "Domain/CoordinateMaps/TimeDependent/ShapeMapTransitionFunctions/ShapeMapTransitionFunction.hpp"
 #include "NumericalAlgorithms/SphericalHarmonics/YlmSpherepack.hpp"
@@ -45,15 +48,27 @@ namespace domain::CoordinateMaps::TimeDependent {
  * coordinates \f$\xi^i\f$ to coordinates \f$x^i\f$:
  *
  * \f{equation}{
- * x^i = \xi^i - (\xi^i - x_c^i) f(r, \theta, \phi) \sum_{lm}
- * \lambda_{lm}(t)Y_{lm}(\theta, \phi).
+ * x^i = \xi^i - (\xi^i - x_c^i) f(r,\theta,\phi) \lambda(\theta,\phi,t)
  * \f}
+ *
+ * where $\lambda(\theta,\phi,t) = \sum_{lm}\lambda_{lm}(t)Y_{lm}(\theta,\phi)$
+ * is the radial distortion and $f(r,\theta,\phi)$ is the transition function.
+ *
+ * This means a coordinate sphere of original radius $r$ (in the unmapped
+ * coordinates $\xi^i$) is mapped to a distorted surface with radius
+ *
+ * \begin{equation}
+ * \tilde{r}(r,\theta,\phi,t) = r \left(1 -
+ *   f(r,\theta,\phi) \lambda(\theta,\phi,t)\right)
+ * \end{equation}
+ *
+ * in the new coordinates $x^i$.
  *
  * The inverse map is given by:
  * \f{equation}{
  * \xi^i = x_c^i + (x^i-x_c^i)*(r/\tilde{r}),
  * \f}
- * where \f$\tilde{r}\f$ is the radius of \f$\xi\f$, calculated by the
+ * where \f$r/\tilde{r}=1/(1-f\lambda)\f$ is calculated by the
  * transition map. For more details, see
  * ShapeMapTransitionFunction::original_radius_over_radius .
  *
@@ -87,11 +102,52 @@ class Shape {
   using FunctionsOfTimeMap = std::unordered_map<
       std::string, std::unique_ptr<domain::FunctionsOfTime::FunctionOfTime>>;
 
+  /*!
+   * \brief Construct a time-dependent Shape map
+   *
+   * \param center The coordinate center of the spherical harmonic expansion
+   * \param l_max $\ell_\mathrm{max}$ of the spherical harmonic expansion
+   * \param m_max $m_\mathrm{max}$ of the spherical harmonic expansion
+   * \param transition_func The ShapeMapTransitionFunction $f(r,\theta,\phi)$
+   * that may reduce the effect of the shape map to the identity at a boundary
+   * \param function_of_time_name Identifies a FunctionOfTime that provides the
+   * modes $\lambda_{lm}$ of the spherical harmonic expansion. See other
+   * constructor for details on the form of the coefficients.
+   */
   explicit Shape(
       const std::array<double, 3>& center, size_t l_max, size_t m_max,
       std::unique_ptr<ShapeMapTransitionFunctions::ShapeMapTransitionFunction>
           transition_func,
       std::string function_of_time_name);
+
+  /*!
+   * \brief Construct a static Shape map
+   *
+   * \param center The coordinate center of the spherical harmonic expansion
+   * \param l_max $\ell_\mathrm{max}$ of the spherical harmonic expansion
+   * \param m_max $m_\mathrm{max}$ of the spherical harmonic expansion
+   * \param transition_func The ShapeMapTransitionFunction $f(r,\theta,\phi)$
+   * that may reduce the effect of the shape map to the identity at a boundary
+   * \param coefs Modes $\lambda_{lm}$ of the spherical harmonic expansion that
+   * define the shape, in YlmSpherepack format. These modes should describe the
+   * radial distortion
+   *
+   * \begin{equation}
+   * \lambda(\theta,\phi) = 1 - \tilde{r}(\theta,\phi) / r
+   * \end{equation}
+   *
+   * where $r$ is the radius of a coordinate sphere in the original (unmapped)
+   * coordinates, $\tilde{r}(\theta,\phi)$ is the radius of the distorted
+   * surface in the new coordinates, and where we have set $f=1$ on this
+   * surface. Here is an example computing the radial distortion for a Kerr
+   * horizon:
+   *
+   * \snippet shape_map_coefs_kerr_horizon
+   */
+  Shape(const std::array<double, 3>& center, size_t l_max, size_t m_max,
+        std::unique_ptr<ShapeMapTransitionFunctions::ShapeMapTransitionFunction>
+            transition_func,
+        DataVector coefs);
 
   Shape() = default;
   ~Shape() = default;
@@ -130,13 +186,14 @@ class Shape {
   static constexpr size_t dim = 3;
 
  private:
-  std::string f_of_t_name_;
+  std::optional<std::string> f_of_t_name_;
   std::array<double, 3> center_{};
   size_t l_max_ = 2;
   size_t m_max_ = 2;
   YlmSpherepack ylm_{2, 2};
   std::unique_ptr<ShapeMapTransitionFunctions::ShapeMapTransitionFunction>
       transition_func_;
+  mutable DataVector coefs_;
 
   template <typename T>
   std::array<tt::remove_cvref_wrap_t<T>, 3> center_coordinates(
