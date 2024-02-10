e971c9ab827f83aaf4e6ff3d448b6188d591ed9e
Nils Vu <nils.vu@black-holes.org>
2024-02-09 16:32:06 -0800

    fixup find paraview

--- cmake/SetupParaView.cmake
+++ cmake/SetupParaView.cmake
@@ -1,4 +1,13 @@
 # Distributed under the MIT License.
 # See LICENSE.txt for details.
 
-find_package(ParaView)
+option(ENABLE_PARAVIEW "Try to find ParaView to enable 3D rendering tools" ON)
+
+if (ENABLE_PARAVIEW)
+  find_package(ParaView)
+endif()
+
+# Help `find_python_module` find ParaView
+if (PARAVIEW_PYTHONPATH)
+  set(PY_paraview_LOCATION ${PARAVIEW_PYTHONPATH}/paraview)
+endif()
