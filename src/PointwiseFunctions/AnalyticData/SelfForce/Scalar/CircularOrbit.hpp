18f46d5561e64aac9da86db5b4160bf4748e4c5f
Nils Vu <owls@nilsvu.de>
2024-07-19 19:31:21 +0200

    Add circular orbit problem for scalar self force

---
+++ src/PointwiseFunctions/AnalyticData/SelfForce/Scalar/CircularOrbit.hpp
@@ -0,0 +1,149 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <array>
+#include <cstddef>
+#include <limits>
+#include <pup.h>
+#include <vector>
+
+#include "DataStructures/DataBox/Prefixes.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Elliptic/Systems/SelfForce/Scalar/Tags.hpp"
+#include "NumericalAlgorithms/LinearOperators/PartialDerivatives.hpp"
+#include "NumericalAlgorithms/Spectral/Mesh.hpp"
+#include "Options/String.hpp"
+#include "PointwiseFunctions/InitialDataUtilities/Background.hpp"
+#include "PointwiseFunctions/InitialDataUtilities/InitialGuess.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/TMPL.hpp"
+#include "Utilities/TaggedTuple.hpp"
+
+namespace ScalarSelfForce::AnalyticData {
+
+/*!
+ * \brief Scalar self force for a scalar charge on a circular equatorial orbit.
+ *
+ * This class implements Eq. (2.9) in \cite Osburn2022 . It does so by defining
+ * the background fields $\alpha$, $\beta$, and $\gamma_i$ in the general form
+ * of the equations
+ * \begin{equation}
+ * -\partial_i F^i + \beta \Psi_m + \gamma_i F^i = S_m
+ * \text{.}
+ * \end{equation}
+ * with the flux
+ * \begin{equation}
+ * F^i = \{\partial_{r_\star}, \alpha \partial_{\cos\theta}\} \Psi_m
+ * \text{.}
+ * \end{equation}
+ * Note that we use $\cos\theta$ as angular coordinate but \cite Osburn2022 uses
+ * $\theta$. We also multiply Eq. (2.9) by the factor $\Sigma^2 / (r^2 + a^2)^2$
+ * so we can easily write it in first-order flux form. The resulting factors in
+ * the equation are:
+ *
+ * \begin{align}
+ * &\alpha = \frac{\Delta}{(r^2 + a^2)^2}
+ * &\beta = \left(-m^2\Omega^2 \Sigma^2 + 4a m^2 \Omega M r + \Delta \left[
+ *   \frac{m^2}{\sin^2\theta} + \frac{2M}{r}(1-\frac{a^2}{Mr}) + \frac{2iam}{r}
+ *   \right]\right) \frac{1}{(r^2 + a^2)^2}}
+ * &\gamma_{r_\star} = -\frac{2iam}{r^2+a^2} + \frac{2a^2}{r}\alpha
+ * &\gamma_{\cos\theta} = 0
+ * \end{equation}
+ *
+ * This class also provides the effective source $S_m^\mathrm{eff}$ for the
+ * equations when they apply to the regular field $S_m^\mathcal{R}$ at or near
+ * the scalar point charge (see Sec. III in \cite Osburn2022 ). The effective
+ * source is computed using the EffectiveSource code by \cite Wardell??? .
+ * It is then transformed to correspond to the m-mode decomposition used in
+ * \cite Osburn2022 .
+ */
+class CircularOrbit : public elliptic::analytic_data::Background,
+                      public elliptic::analytic_data::InitialGuess {
+ public:
+  struct BlackHoleMass {
+    static constexpr Options::String help =
+        "Kerr mass parameter 'M' of the black hole";
+    using type = double;
+  };
+  struct BlackHoleSpin {
+    static constexpr Options::String help =
+        "Kerr dimensionless spin parameter 'chi' of the black hole";
+    using type = double;
+  };
+  struct OrbitalRadius {
+    static constexpr Options::String help =
+        "Radius 'r_0' of the circular orbit";
+    using type = double;
+  };
+  struct MModeNumber {
+    static constexpr Options::String help =
+        "Mode number 'm' of the scalar field";
+    using type = int;
+  };
+  using options =
+      tmpl::list<BlackHoleMass, BlackHoleSpin, OrbitalRadius, MModeNumber>;
+  static constexpr Options::String help =
+      "Quasicircular orbit of a scalar point charge in Kerr spacetime";
+
+  CircularOrbit() = default;
+  CircularOrbit(const CircularOrbit&) = default;
+  CircularOrbit& operator=(const CircularOrbit&) = default;
+  CircularOrbit(CircularOrbit&&) = default;
+  CircularOrbit& operator=(CircularOrbit&&) = default;
+  ~CircularOrbit() = default;
+
+  CircularOrbit(double black_hole_mass, double black_hole_spin,
+                double orbital_radius, int m_mode_number);
+
+  explicit CircularOrbit(CkMigrateMessage* m)
+      : elliptic::analytic_data::Background(m),
+        elliptic::analytic_data::InitialGuess(m) {}
+  using PUP::able::register_constructor;
+  WRAPPED_PUPable_decl_template(CircularOrbit);
+
+  tnsr::I<double, 2> puncture_position() const;
+
+  using background_tags = tmpl::list<Tags::Alpha, Tags::Beta, Tags::Gamma>;
+  using source_tags = tmpl::list<
+      ::Tags::FixedSource<Tags::MMode>, Tags::SingularField,
+      ::Tags::deriv<Tags::SingularField, tmpl::size_t<2>, Frame::Inertial>,
+      Tags::BoyerLindquistRadius>;
+
+  // Background
+  tuples::tagged_tuple_from_typelist<background_tags> variables(
+      const tnsr::I<DataVector, 2>& x, background_tags /*meta*/) const;
+
+  // Initial guess
+  tuples::TaggedTuple<Tags::MMode> variables(
+      const tnsr::I<DataVector, 2>& x, tmpl::list<Tags::MMode> /*meta*/) const;
+
+  // Fixed sources
+  tuples::tagged_tuple_from_typelist<source_tags> variables(
+      const tnsr::I<DataVector, 2>& x, source_tags /*meta*/) const;
+
+  template <typename... RequestedTags>
+  tuples::TaggedTuple<RequestedTags...> variables(
+      const tnsr::I<DataVector, 2>& x, const Mesh<2>& /*mesh*/,
+      const InverseJacobian<DataVector, 2, Frame::ElementLogical,
+                            Frame::Inertial>& /*inv_jacobian*/,
+      tmpl::list<RequestedTags...> /*meta*/) const {
+    return variables(x, tmpl::list<RequestedTags...>{});
+  }
+
+  // NOLINTNEXTLINE
+  void pup(PUP::er& p) override;
+
+ private:
+  friend bool operator==(const CircularOrbit& lhs, const CircularOrbit& rhs);
+
+  double black_hole_mass_{std::numeric_limits<double>::signaling_NaN()};
+  double black_hole_spin_{std::numeric_limits<double>::signaling_NaN()};
+  double orbital_radius_{std::numeric_limits<double>::signaling_NaN()};
+  int m_mode_number_{};
+};
+
+bool operator!=(const CircularOrbit& lhs, const CircularOrbit& rhs);
+
+}  // namespace ScalarSelfForce::AnalyticData
