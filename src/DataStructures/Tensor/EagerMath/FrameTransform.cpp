8f9d4b2e807b62674d142867cfb1b224cd6eba2c
Nils Vu <owls@nilsvu.de>
2025-09-04 15:36:31 -0700

    Instantiate some more mixed-dimension functions

--- src/DataStructures/Tensor/EagerMath/FrameTransform.cpp
+++ src/DataStructures/Tensor/EagerMath/FrameTransform.cpp
@@ -428,6 +428,16 @@ GENERATE_INSTANTIATIONS(INSTANTIATE, (1, 2, 3), (Frame::ElementLogical),
                          ComplexDataVector),
                         (I, II, Ij, IJ, Iaa))
 
+template auto transform::first_index_to_different_frame(
+    const TensorMetafunctions::prepend_spatial_index<
+        tnsr::aa<ComplexDataVector, 3, Frame::Inertial>, 2, UpLo::Up,
+        Frame::Inertial>& input,
+    const InverseJacobian<DataVector, 2, Frame::ElementLogical,
+                          Frame::Inertial>& inv_jacobian)
+    -> TensorMetafunctions::prepend_spatial_index<
+        tnsr::aa<ComplexDataVector, 3, Frame::Inertial>, 2, UpLo::Up,
+        Frame::ElementLogical>;
+
 #undef DIM
 #undef SRCFRAME
 #undef DESTFRAME
