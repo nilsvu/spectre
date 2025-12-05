160a71f1c059e4a69a7aa6a47d7bdf993efff9a6
Nils Vu <owls@nilsvu.de>
2025-12-05 11:51:21 +0000

    Auto-fetch Eigen

--- cmake/SetupEigen.cmake
+++ cmake/SetupEigen.cmake
@@ -3,8 +3,28 @@
 
 find_package(Eigen3)
 
-target_compile_definitions(
-  Eigen3::Eigen
-  INTERFACE
-  EIGEN_DONT_PARALLELIZE
-)
+if (NOT Eigen3_FOUND)
+  if (NOT SPECTRE_FETCH_MISSING_DEPS)
+    message(FATAL_ERROR "Could not find Eigen. If you want to fetch "
+      "missing dependencies automatically, set SPECTRE_FETCH_MISSING_DEPS=ON.")
+  endif()
+  message(STATUS "Fetching Eigen")
+  include(FetchContent)
+  FetchContent_Declare(Eigen3
+    GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
+    GIT_TAG 3.4.0
+    GIT_SHALLOW TRUE
+    ${SPECTRE_FETCHCONTENT_BASE_ARGS}
+  )
+  set(EIGEN_BUILD_TESTING OFF)
+  set(EIGEN_BUILD_PKGCONFIG OFF)
+  set(EIGEN_BUILD_DOC OFF)
+  set(EIGEN_DONT_PARALLELIZE ON)
+  FetchContent_MakeAvailable(Eigen3)
+else()
+  target_compile_definitions(
+    Eigen3::Eigen
+    INTERFACE
+    EIGEN_DONT_PARALLELIZE
+  )
+endif()
