8e5aca76da61800f46da5869b2cbc67c5c8c41f3
Nils Vu <owls@nilsvu.de>
2025-12-12 09:46:13 -0800

    WIP scalar

---
+++ support/Pipelines/SelfForce/Scalar/kernels.py
@@ -0,0 +1,12 @@
+from spectre.IO.Exporter import interpolate_to_points, ObservationStep
+from spectre.DataStructures.Tensor import Scalar, DataVector, tnsr
+
+ref_fname = "/Users/nilsvu/Projects/spectre/build-Default-Release/test_ssf45/m2/ScalarSelfForceVolume0.h5"
+
+def error(field: Scalar[DataVector], x: tnsr.I[DataVector, 2]) -> DataVector:
+    ref_data, = interpolate_to_points(ref_fname, "VolumeData", observation=ObservationStep(-1), tensor_components=["Re(MMode)"],
+    target_points=x)
+    m = 1
+    z = x[1]
+    return (field.get() #* (1 - z**2)**(m/2)
+            - ref_data)
